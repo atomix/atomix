7de5988e5407851307472aedb6981975940600da
Jordan Halterman <jordan.halterman@gmail.com>
2014-11-28 03:39:54 -0800

    Add Copycat context.

---
+++ core/src/main/java/net/kuujo/copycat/CopycatContext.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kuujo.copycat;
+
+import net.kuujo.copycat.cluster.Member;
+
+/**
+ * Copycat context.
+ *
+ * @author <a href="http://github.com/kuujo">Jordan Halterman</a>
+ */
+public interface CopycatContext {
+
+  /**
+   * Returns the current cluster term.
+   *
+   * @return The current cluster term.
+   */
+  long term();
+
+  /**
+   * Returns the current cluster leader.
+   *
+   * @return The current cluster leader.
+   */
+  Member leader();
+
+  /**
+   * Returns a boolean indicating whether the local node is the current cluster leader.
+   *
+   * @return Indicates whether the local node is the current cluster leader.
+   */
+  boolean isLeader();
+
+}
