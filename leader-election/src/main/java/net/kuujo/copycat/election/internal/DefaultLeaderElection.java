740ed44bd00a682a17e0ea8c4a48c03b290721e3
Jordan Halterman <jordan.halterman@gmail.com>
2014-12-23 23:58:40 -0800

    Ensure leader election handler is executed on user thread.

--- leader-election/src/main/java/net/kuujo/copycat/election/internal/DefaultLeaderElection.java
+++ leader-election/src/main/java/net/kuujo/copycat/election/internal/DefaultLeaderElection.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.kuujo.copycat.election.internal;
 
 import net.kuujo.copycat.CopycatContext;
@@ -13,13 +28,15 @@ import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
 /**
+ * Default leader election implementation.
+ *
  * @author <a href="http://github.com/kuujo">Jordan Halterman</a>
  */
 public class DefaultLeaderElection extends AbstractCopycatResource<LeaderElection> implements LeaderElection {
   private Consumer<Member> handler;
   private final Consumer<ElectionResult> electionListener = result -> {
     if (handler != null) {
-      handler.accept(result.winner());
+      executor.execute(() -> handler.accept(result.winner()));
     }
   };
 
