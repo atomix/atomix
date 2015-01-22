f35604b003853ee24a08e7b6ad7f2a28450bb525
Jordan Halterman <jordan.halterman@gmail.com>
2015-01-21 19:50:41 -0800

    Refactor Vert.x 2 event bus protocol to adhere to configurability requirements.

--- vertx/src/main/java/net/kuujo/copycat/vertx/VertxEventBusProtocol.java
+++ vertx/src/main/java/net/kuujo/copycat/vertx/VertxEventBusProtocol.java
@@ -15,6 +15,7 @@
  */
 package net.kuujo.copycat.vertx;
 
+import net.kuujo.copycat.internal.util.Assert;
 import net.kuujo.copycat.protocol.AbstractProtocol;
 import net.kuujo.copycat.protocol.ProtocolClient;
 import net.kuujo.copycat.protocol.ProtocolServer;
@@ -22,6 +23,7 @@ import org.vertx.java.core.Vertx;
 import org.vertx.java.core.VertxFactory;
 
 import java.net.URI;
+import java.util.Map;
 
 /**
  * Vert.x event bus protocol implementation.
@@ -29,24 +31,146 @@ import java.net.URI;
  * @author <a href="http://github.com/kuujo">Jordan Halterman</a>
  */
 public class VertxEventBusProtocol extends AbstractProtocol {
-  private final Vertx vertx;
+  public static final String VERTX = "vertx";
+  public static final String VERTX_HOST = "host";
+  public static final String VERTX_PORT = "port";
+
+  private static final String DEFAULT_VERTX_HOST = "localhost";
+  private static final int DEFAULT_VERTX_PORT = 0;
+
+  public VertxEventBusProtocol() {
+    super();
+  }
 
   public VertxEventBusProtocol(String host, int port) {
-    this(VertxFactory.newVertx(port, host));
+    super();
+    setHost(host);
+    setPort(port);
   }
 
   public VertxEventBusProtocol(Vertx vertx) {
-    this.vertx = vertx;
+    setVertx(vertx);
   }
 
-  @Override
-  public ProtocolClient createClient(URI uri) {
-    return new VertxEventBusProtocolClient(uri.getAuthority(), vertx);
+  public VertxEventBusProtocol(Map<String, Object> config) {
+    super(config);
+  }
+
+  /**
+   * Sets the Vert.x instance.
+   *
+   * @param vertx The Vert.x instance.
+   */
+  public void setVertx(Vertx vertx) {
+    put(VERTX, Assert.isNotNull(vertx, "vertx"));
+  }
+
+  /**
+   * Returns the Vert.x instance.
+   *
+   * @return The Vert.x instance.
+   */
+  public Vertx getVertx() {
+    return get(VERTX);
+  }
+
+  /**
+   * Sets the Vert.x instance, returning the protocol for method chaining.
+   *
+   * @param vertx The Vert.x instance.
+   * @return The event bus protocol.
+   */
+  public VertxEventBusProtocol withVertx(Vertx vertx) {
+    setVertx(vertx);
+    return this;
+  }
+
+  /**
+   * Sets the Vert.x host.
+   *
+   * @param host The Vert.x host.
+   * @throws java.lang.NullPointerException If the host is {@code null}
+   */
+  public void setHost(String host) {
+    put(VERTX_HOST, Assert.isNotNull(host, "host"));
+  }
+
+  /**
+   * Returns the Vert.x host.
+   *
+   * @return The Vert.x host.
+   */
+  public String getHost() {
+    return get(VERTX_HOST, DEFAULT_VERTX_HOST);
+  }
+
+  /**
+   * Sets the Vert.x host, returning the event bus protocol for method chaining.
+   *
+   * @param host The Vert.x host.
+   * @return The event bus protocol.
+   */
+  public VertxEventBusProtocol withHost(String host) {
+    setHost(host);
+    return this;
+  }
+
+  /**
+   * Sets the Vert.x port.
+   *
+   * @param port The Vert.x port.
+   */
+  public void setPort(int port) {
+    put(VERTX_PORT, Assert.arg(port, port > -1, "port must be positive"));
+  }
+
+  /**
+   * Returns the Vert.x port.
+   *
+   * @return The Vert.x port.
+   */
+  public int getPort() {
+    return get(VERTX_PORT, DEFAULT_VERTX_PORT);
+  }
+
+  /**
+   * Sets the Vert.x port, returning the protocol for method chaining.
+   *
+   * @param port The Vert.x port.
+   * @return The event bus protocol.
+   */
+  public VertxEventBusProtocol withPort(int port) {
+    setPort(port);
+    return this;
+  }
+
+  /**
+   * Creates a new Vert.x instance.
+   */
+  private Vertx createVertx() {
+    String host = getHost();
+    int port = getPort();
+    return host != null ? VertxFactory.newVertx(port, host) : VertxFactory.newVertx();
   }
 
   @Override
   public ProtocolServer createServer(URI uri) {
-    return new VertxEventBusProtocolServer(uri.getAuthority(), vertx);
+    Vertx vertx = getVertx();
+    if (vertx != null) {
+      return new VertxEventBusProtocolServer(uri.getAuthority(), vertx);
+    } else {
+      return new VertxEventBusProtocolServer(uri.getAuthority(), createVertx());
+    }
+  }
+
+  @Override
+  public ProtocolClient createClient(URI uri) {
+    Vertx vertx = getVertx();
+    if (vertx != null) {
+      return new VertxEventBusProtocolClient(uri.getAuthority(), vertx);
+    } else {
+      return new VertxEventBusProtocolClient(uri.getAuthority(), createVertx());
+    }
   }
 
 }
