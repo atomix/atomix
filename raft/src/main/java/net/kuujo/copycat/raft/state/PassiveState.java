19a49f8f36b2f6d82534dc13504d672e41a3a8d1
Jordan Halterman <jordan.halterman@gmail.com>
2015-06-10 02:01:29 -0700

    Move AppendEntries handling to PASSIVE state.

--- raft/src/main/java/net/kuujo/copycat/raft/state/PassiveState.java
+++ raft/src/main/java/net/kuujo/copycat/raft/state/PassiveState.java
@@ -16,6 +16,7 @@
 package net.kuujo.copycat.raft.state;
 
 import net.kuujo.copycat.raft.RaftError;
+import net.kuujo.copycat.raft.log.entry.Entry;
 import net.kuujo.copycat.raft.rpc.*;
 
 import java.util.concurrent.CompletableFuture;
@@ -26,6 +27,7 @@ import java.util.concurrent.CompletableFuture;
  * @author <a href="http://github.com/kuujo">Jordan Halterman</a>
  */
 public class PassiveState extends AbstractState {
+  protected boolean transition;
 
   public PassiveState(RaftStateContext context) {
     super(context);
@@ -36,14 +38,205 @@ public class PassiveState extends AbstractState {
     return RaftState.PASSIVE;
   }
 
+  /**
+   * Transitions to a new state.
+   */
+  protected void transition(RaftState state) {
+    // Do not allow the PASSIVE state to transition.
+  }
+
   @Override
-  protected CompletableFuture<AppendResponse> append(AppendRequest request) {
+  protected CompletableFuture<AppendResponse> append(final AppendRequest request) {
     context.checkThread();
-    logRequest(request);
-    return CompletableFuture.completedFuture(logResponse(AppendResponse.builder()
-      .withStatus(Response.Status.ERROR)
-      .withError(RaftError.Type.ILLEGAL_MEMBER_STATE_ERROR)
-      .build()));
+    CompletableFuture<AppendResponse> future = CompletableFuture.completedFuture(logResponse(handleAppend(logRequest(request))));
+    // If a transition is required then transition back to the follower state.
+    // If the node is already a follower then the transition will be ignored.
+    if (transition) {
+      transition(RaftState.FOLLOWER);
+      transition = false;
+    }
+    return future;
+  }
+
+  /**
+   * Starts the append process.
+   */
+  private AppendResponse handleAppend(AppendRequest request) {
+    // If the request indicates a term that is greater than the current term then
+    // assign that term and leader to the current context and step down as leader.
+    if (request.term() > context.getTerm() || (request.term() == context.getTerm() && context.getLeader() == 0)) {
+      context.setTerm(request.term());
+      context.setLeader(request.leader());
+      transition = true;
+    }
+
+    // If the request term is less than the current term then immediately
+    // reply false and return our current term. The leader will receive
+    // the updated term and step down.
+    if (request.term() < context.getTerm()) {
+      LOGGER.warn("{} - Rejected {}: request term is less than the current term ({})", context.getCluster().member().id(), request, context.getTerm());
+      return AppendResponse.builder()
+        .withStatus(Response.Status.OK)
+        .withTerm(context.getTerm())
+        .withSucceeded(false)
+        .withLogIndex(context.getLog().lastIndex())
+        .build();
+    } else if (request.logIndex() != 0 && request.logTerm() != 0) {
+      return doCheckPreviousEntry(request);
+    } else {
+      return doAppendEntries(request);
+    }
+  }
+
+  /**
+   * Checks the previous log entry for consistency.
+   */
+  private AppendResponse doCheckPreviousEntry(AppendRequest request) {
+    if (request.logIndex() != 0 && context.getLog().isEmpty()) {
+      LOGGER.warn("{} - Rejected {}: Previous index ({}) is greater than the local log's last index ({})", context.getCluster().member().id(), request, request.logIndex(), context.getLog().lastIndex());
+      return AppendResponse.builder()
+        .withStatus(Response.Status.OK)
+        .withTerm(context.getTerm())
+        .withSucceeded(false)
+        .withLogIndex(context.getLog().lastIndex())
+        .build();
+    } else if (request.logIndex() != 0 && context.getLog().lastIndex() != 0 && request.logIndex() > context.getLog().lastIndex()) {
+      LOGGER.warn("{} - Rejected {}: Previous index ({}) is greater than the local log's last index ({})", context.getCluster().member().id(), request, request.logIndex(), context.getLog().lastIndex());
+      return AppendResponse.builder()
+        .withStatus(Response.Status.OK)
+        .withTerm(context.getTerm())
+        .withSucceeded(false)
+        .withLogIndex(context.getLog().lastIndex())
+        .build();
+    }
+
+    // If the previous entry term doesn't match the local previous term then reject the request.
+    Entry entry = context.getLog().getEntry(request.logIndex());
+    if (entry == null || entry.getTerm() != request.logTerm()) {
+      LOGGER.warn("{} - Rejected {}: Request log term does not match local log term {} for the same entry", context.getCluster().member().id(), request, entry.getTerm());
+      return AppendResponse.builder()
+        .withStatus(Response.Status.OK)
+        .withTerm(context.getTerm())
+        .withSucceeded(false)
+        .withLogIndex(request.logIndex() <= context.getLog().lastIndex() ? request.logIndex() - 1 : context.getLog().lastIndex())
+        .build();
+    } else {
+      return doAppendEntries(request);
+    }
+  }
+
+  /**
+   * Appends entries to the local log.
+   */
+  @SuppressWarnings("unchecked")
+  private AppendResponse doAppendEntries(AppendRequest request) {
+    // If the log contains entries after the request's previous log index
+    // then remove those entries to be replaced by the request entries.
+    if (!request.entries().isEmpty()) {
+
+      // Iterate through request entries and append them to the log.
+      for (Entry entry : request.entries()) {
+        // Replicated snapshot entries are *always* immediately logged and applied to the state machine
+        // since snapshots are only taken of committed state machine state. This will cause all previous
+        // entries to be removed from the log.
+        if (context.getLog().containsIndex(entry.getIndex())) {
+          // Compare the term of the received entry with the matching entry in the log.
+          Entry match = context.getLog().getEntry(entry.getIndex());
+          if (match != null) {
+            if (entry.getTerm() != match.getTerm()) {
+              // We found an invalid entry in the log. Remove the invalid entry and append the new entry.
+              // If appending to the log fails, apply commits and reply false to the append request.
+              LOGGER.warn("{} - Appended entry term does not match local log, removing incorrect entries", context.getCluster().member().id());
+              context.getLog().truncate(entry.getIndex() - 1);
+              context.getLog().appendEntry(entry);
+              LOGGER.debug("{} - Appended {} to log at index {}", context.getCluster().member().id(), entry, entry.getIndex());
+            }
+          } else {
+            context.getLog().truncate(entry.getIndex() - 1);
+            context.getLog().appendEntry(entry);
+            LOGGER.debug("{} - Appended {} to log at index {}", context.getCluster().member().id(), entry, entry.getIndex());
+          }
+        } else {
+          // If appending to the log fails, apply commits and reply false to the append request.
+          context.getLog().skip(entry.getIndex() - context.getLog().lastIndex() - 1).appendEntry(entry);
+          LOGGER.debug("{} - Appended {} to log at index {}", context.getCluster().member().id(), entry, entry.getIndex());
+        }
+      }
+    }
+
+    // If we've made it this far, apply commits and send a successful response.
+    context.getContext().execute(() -> {
+      applyCommits(request.commitIndex())
+        .thenRun(() -> applyIndex(request.globalIndex()))
+        .thenRun(request::close);
+    });
+
+    return AppendResponse.builder()
+      .withStatus(Response.Status.OK)
+      .withTerm(context.getTerm())
+      .withSucceeded(true)
+      .withLogIndex(context.getLog().lastIndex())
+      .build();
+  }
+
+  /**
+   * Applies commits to the local state machine.
+   */
+  @SuppressWarnings("unchecked")
+  private CompletableFuture<Void> applyCommits(long commitIndex) {
+    // If the synced commit index is greater than the local commit index then
+    // apply commits to the local state machine.
+    // Also, it's possible that one of the previous write applications failed
+    // due to asynchronous communication errors, so alternatively check if the
+    // local commit index is greater than last applied. If all the state machine
+    // commands have not yet been applied then we want to re-attempt to apply them.
+    if (commitIndex != 0 && !context.getLog().isEmpty()) {
+      if (context.getCommitIndex() == 0 || commitIndex > context.getCommitIndex()) {
+        long lastIndex = context.getLog().lastIndex();
+        int commits = (int) (Math.min(commitIndex, lastIndex) - Math.max(context.getCommitIndex(), context.getLog().firstIndex()));
+
+        LOGGER.debug("{} - Applying {} commits", context.getCluster().member().id(), commits);
+
+        // Update the local commit index with min(request commit, last log // index)
+        long previousCommitIndex = context.getCommitIndex();
+        if (lastIndex != 0) {
+          context.setCommitIndex(Math.min(Math.max(commitIndex, previousCommitIndex != 0 ? previousCommitIndex : commitIndex), lastIndex));
+
+          // If the updated commit index indicates that commits remain to be
+          // applied to the state machine, iterate entries and apply them.
+          if (context.getCommitIndex() > previousCommitIndex) {
+            // Starting after the last applied entry, iterate through new entries
+            // and apply them to the state machine up to the commit index.
+            CompletableFuture<?>[] futures = new CompletableFuture[commits];
+            int j = 0;
+            for (long i = Math.max(previousCommitIndex + 1, context.getLog().firstIndex()); i <= Math.min(context.getCommitIndex(), lastIndex); i++) {
+              Entry entry = context.getLog().getEntry(i);
+              if (entry != null) {
+                futures[j++] = context.getStateMachine().apply(entry).whenCompleteAsync((result, error) -> {
+                  if (isOpen() && error != null) {
+                    LOGGER.info("{} - An application error occurred: {}", context.getCluster().member().id(), error);
+                  }
+                  entry.close();
+                }, context.getContext());
+              }
+            }
+            return CompletableFuture.allOf(futures);
+          }
+        }
+      }
+    } else {
+      context.setCommitIndex(commitIndex);
+    }
+    return CompletableFuture.completedFuture(null);
+  }
+
+  /**
+   * Recycles the log up to the given index.
+   */
+  private void applyIndex(long globalIndex) {
+    if (globalIndex > 0) {
+      context.setGlobalIndex(globalIndex);
+    }
   }
 
   @Override
