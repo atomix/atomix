eaddb4612b301fa14682280bb986d790bda6dc44
Jordan Halterman <jordan.halterman@gmail.com>
2015-07-22 19:27:52 -0700

    Rename 'test' transport to 'local'.

---
+++ local/src/main/java/net/kuujo/copycat/transport/LocalTransport.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kuujo.copycat.transport;
+
+import net.kuujo.alleycat.Alleycat;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Local transport.
+ *
+ * @author <a href="http://github.com/kuujo">Jordan Halterman</a>
+ */
+public class LocalTransport implements Transport {
+
+  /**
+   * Returns a new local transport builder.
+   *
+   * @return A new local transport builder.
+   */
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  private final LocalServerRegistry registry;
+  private final Alleycat serializer;
+  private final Map<UUID, LocalClient> clients = new ConcurrentHashMap<>();
+  private final Map<UUID, LocalServer> servers = new ConcurrentHashMap<>();
+
+  private LocalTransport(LocalServerRegistry registry, Alleycat serializer) {
+    this.registry = registry;
+    this.serializer = serializer;
+  }
+
+  @Override
+  public Client client(UUID id) {
+    return clients.computeIfAbsent(id, i -> new LocalClient(id, registry, serializer));
+  }
+
+  @Override
+  public Server server(UUID id) {
+    return servers.computeIfAbsent(id, i -> new LocalServer(id, registry, serializer));
+  }
+
+  @Override
+  public CompletableFuture<Void> close() {
+    int i = 0;
+
+    CompletableFuture[] futures = new CompletableFuture[clients.size() + servers.size()];
+    for (Client client : clients.values()) {
+      futures[i++] = client.close();
+    }
+
+    for (Server server : servers.values()) {
+      futures[i++] = server.close();
+    }
+
+    return CompletableFuture.allOf(futures);
+  }
+
+  /**
+   * Local transport builder.
+   */
+  public static class Builder extends Transport.Builder {
+    private LocalServerRegistry registry;
+    private Alleycat serializer;
+
+    /**
+     * Sets the transport server registry.
+     *
+     * @param registry The local server registry.
+     * @return The transport builder.
+     */
+    public Builder withRegistry(LocalServerRegistry registry) {
+      if (registry == null)
+        throw new NullPointerException("registry cannot be null");
+      this.registry = registry;
+      return this;
+    }
+
+    /**
+     * Sets the transport serializer.
+     *
+     * @param serializer The transport serializer.
+     * @return The transport builder.
+     */
+    public Builder withSerializer(Alleycat serializer) {
+      this.serializer = serializer;
+      return this;
+    }
+
+    @Override
+    public Transport build() {
+      return new LocalTransport(registry, serializer != null ? serializer : new Alleycat());
+    }
+  }
+
+}
---
+++ local/src/test/java/net/kuujo/copycat/transport/LocalTransportTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kuujo.copycat.transport;
+
+import net.jodah.concurrentunit.ConcurrentTestCase;
+import net.kuujo.alleycat.Alleycat;
+import net.kuujo.copycat.util.concurrent.Context;
+import net.kuujo.copycat.util.concurrent.SingleThreadContext;
+import org.testng.annotations.Test;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Local transport test.
+ *
+ * @author <a href="http://github.com/kuujo">Jordan Halterman</a>
+ */
+@Test
+public class LocalTransportTest extends ConcurrentTestCase {
+
+  /**
+   * Tests connecting to a server and sending a message.
+   */
+  public void testSendReceive() throws Throwable {
+    LocalServerRegistry registry = new LocalServerRegistry();
+
+    Transport clientTransport = LocalTransport.builder()
+      .withRegistry(registry)
+      .build();
+
+    Transport serverTransport = LocalTransport.builder()
+      .withRegistry(registry)
+      .build();
+
+    Server server = serverTransport.server(UUID.randomUUID());
+    Client client = clientTransport.client(UUID.randomUUID());
+
+    Context context = new SingleThreadContext("test-thread-%d", new Alleycat());
+
+    expectResume();
+    context.execute(() -> {
+      try {
+        server.listen(new InetSocketAddress(InetAddress.getByName("localhost"), 5555), connection -> {
+          connection.<String, String>handler(String.class, message -> {
+            threadAssertEquals("Hello world!", message);
+            return CompletableFuture.completedFuture("Hello world back!");
+          });
+        }).thenRun(this::resume);
+      } catch (UnknownHostException e) {
+        threadFail(e);
+      }
+    });
+    await();
+
+    expectResume();
+    context.execute(() -> {
+      try {
+        client.connect(new InetSocketAddress(InetAddress.getByName("localhost"), 5555)).thenAccept(connection -> {
+          connection.send("Hello world!").thenAccept(response -> {
+            threadAssertEquals("Hello world back!", response);
+            resume();
+          });
+        });
+      } catch (UnknownHostException e) {
+        threadFail(e);
+      }
+    });
+    await();
+  }
+
+}
