62a764ae9eae296436576440630391ebb307c282
Jordan Halterman <jordan.halterman@gmail.com>
2015-07-26 04:59:43 -0700

    Move AsyncTopic to collections module.

--- collections/src/main/java/net/kuujo/copycat/collections/AsyncTopic.java
+++ collections/src/main/java/net/kuujo/copycat/collections/AsyncTopic.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.kuujo.copycat.queue;
+package net.kuujo.copycat.collections;
 
 import net.kuujo.alleycat.Alleycat;
 import net.kuujo.alleycat.AlleycatSerializable;
