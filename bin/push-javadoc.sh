b6daeabf2087f787be0430999e4f4a4e31d4cefd
Jonathan Halterman <jhalterman@gmail.com>
2015-07-28 16:55:39 -0700

    Add support for versioned javadoc generation

--- bin/push-javadoc.sh
+++ bin/push-javadoc.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # run from top level dir
+
+echo "Enter the API version to generate docs for: "
+read apiVersion
+
 rm -rf target/docs
 git clone git@github.com:kuujo/copycat.git target/docs -b gh-pages
-mvn -Pjavadoc javadoc:javadoc
+mvn javadoc:javadoc -Pjavadoc -Djv=$apiVersion
 cd target/docs
 git add -A
 git commit -m "Updated JavaDocs"
