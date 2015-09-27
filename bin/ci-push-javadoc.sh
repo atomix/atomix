3d904b249d043ccc20f89388691621f01a925b30
Jonathan Halterman <jhalterman@gmail.com>
2015-09-27 09:12:59 -0700

    push javadoc update

--- bin/ci-push-javadoc.sh
+++ bin/ci-push-javadoc.sh
@@ -7,18 +7,19 @@ if [ "$TRAVIS_REPO_SLUG" == "atomix/$PROJECT" ] && \
    [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
-  echo -e "Publishing javadoc...\n"
+  echo -e "Publishing Javadoc...\n"
   
   mvn javadoc:javadoc -Djv=latest
   TARGET="$(pwd)/target"
 
   cd $HOME
-  git clone --quiet https://${GH_TOKEN}@github.com/atomix.github.io gh-pages > /dev/null
+  git clone --quiet https://${GH_TOKEN}@github.com/atomix/atomix.github.io gh-pages > /dev/null
   
   cd gh-pages
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git rm -rf $PROJECT/api/latest 
+  mkdir -p $PROJECT/api/latest
   mv ${TARGET}/site/apidocs/api/latest $PROJECT/api
   git add -A -f $PROJECT/api/latest
   git commit -m "Javadoc generated from $PROJECT travis build $TRAVIS_BUILD_NUMBER"
