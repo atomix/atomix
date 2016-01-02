0cab99e73ebe58c6ac03a5d897f584082a070b03
Jordan Halterman <jordan.halterman@gmail.com>
2016-01-02 02:35:31 -0800

    Move InstanceManager test back to atomix module.

--- manager/src/test/java/io/atomix/resource/InstanceManagerTest.java
+++ manager/src/test/java/io/atomix/resource/InstanceManagerTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
-package resource;
+package io.atomix.resource;
 
 import io.atomix.copycat.client.CopycatClient;
 import io.atomix.resource.InstanceManager;
