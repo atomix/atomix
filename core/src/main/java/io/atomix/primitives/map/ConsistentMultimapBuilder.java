1bbb523174f1e425ed5104105e7e5b7c1ec4991a
Jordan Halterman <jordan.halterman@gmail.com>
2017-08-29 10:19:21 -0700

    Update copyright headers for ONF

--- core/src/main/java/io/atomix/primitives/map/ConsistentMultimapBuilder.java
+++ core/src/main/java/io/atomix/primitives/map/ConsistentMultimapBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Open Networking Laboratory
+ * Copyright 2016 Open Networking Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
