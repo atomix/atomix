eadfd9fca668ee76b25989f8a13df72c925431f8
Jordan Halterman <jordan.halterman@gmail.com>
2017-09-12 15:03:48 -0700

    Ensure segment file loading is backwards compatible.

--- storage/journal/src/test/java/io/atomix/storage/journal/JournalSegmentFileTest.java
+++ storage/journal/src/test/java/io/atomix/storage/journal/JournalSegmentFileTest.java
@@ -32,16 +32,13 @@ public class JournalSegmentFileTest {
   public void testIsSegmentFile() throws Exception {
     assertTrue(JournalSegmentFile.isSegmentFile("foo", "foo-1.log"));
     assertFalse(JournalSegmentFile.isSegmentFile("foo", "bar-1.log"));
-    assertFalse(JournalSegmentFile.isSegmentFile("foo", "foo-1-1.log"));
+    assertTrue(JournalSegmentFile.isSegmentFile("foo", "foo-1-1.log"));
   }
 
   @Test
   public void testCreateSegmentFile() throws Exception {
     File file = JournalSegmentFile.createSegmentFile("foo", new File(System.getProperty("user.dir")), 1);
     assertTrue(JournalSegmentFile.isSegmentFile("foo", file));
-
-    JournalSegmentFile segmentFile = new JournalSegmentFile(file);
-    assertEquals(1, segmentFile.id());
   }
 
 }
