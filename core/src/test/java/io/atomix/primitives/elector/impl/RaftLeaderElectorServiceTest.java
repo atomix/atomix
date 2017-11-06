8a9b73956378fcc2edc8e0208846036457e6edeb
Jordan Halterman <jordan.halterman@gmail.com>
2017-11-05 19:01:39 -0800

    Move leader elector tests to the correct package.

--- core/src/test/java/io/atomix/primitives/elector/impl/RaftLeaderElectorServiceTest.java
+++ core/src/test/java/io/atomix/primitives/elector/impl/RaftLeaderElectorServiceTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.atomix.elector.impl;
+package io.atomix.primitives.elector.impl;
 
 import io.atomix.cluster.NodeId;
 import io.atomix.leadership.Leadership;
