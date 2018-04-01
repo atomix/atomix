18b474c1ca2f3a99a06a0942ee6d0b2f6bab0622
Jordan Halterman <jordan.halterman@gmail.com>
2018-04-01 01:04:22 -0700

    Skip empty keep-alives when resetting indexes.

--- protocols/raft/src/main/java/io/atomix/protocols/raft/proxy/impl/RaftProxyManager.java
+++ protocols/raft/src/main/java/io/atomix/protocols/raft/proxy/impl/RaftProxyManager.java
@@ -244,6 +244,11 @@ public class RaftProxyManager {
   private synchronized void resetAllIndexes() {
     Collection<RaftProxyState> sessions = Lists.newArrayList(this.sessions.values());
 
+    // If no sessions are open, skip the keep-alive.
+    if (sessions.isEmpty()) {
+      return;
+    }
+
     // Allocate session IDs, command response sequence numbers, and event index arrays.
     long[] sessionIds = new long[sessions.size()];
     long[] commandResponses = new long[sessions.size()];
