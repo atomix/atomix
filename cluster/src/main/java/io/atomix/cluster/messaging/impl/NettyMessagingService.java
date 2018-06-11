2920a34eed0554792e9d1872cf0b0f82246a1993
Jordan Halterman <jordan.halterman@gmail.com>
2018-06-11 00:53:06 -0700

    Remove another timeout future cancellation. (#628)

--- cluster/src/main/java/io/atomix/cluster/messaging/impl/NettyMessagingService.java
+++ cluster/src/main/java/io/atomix/cluster/messaging/impl/NettyMessagingService.java
@@ -1087,7 +1087,6 @@ public class NettyMessagingService implements ManagedMessagingService {
     @Override
     public void close() {
       if (closed.compareAndSet(false, true)) {
-        timeoutFuture.cancel(false);
         for (Callback callback : futures.values()) {
           callback.completeExceptionally(new ConnectException());
         }
