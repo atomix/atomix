b44c67c9043fb2cefb9ac970d9e657ca78fb1501
Jordan Halterman <jordan.halterman@gmail.com>
2018-06-22 16:28:04 -0700

    Simplify AtomicCounterService implementation.

--- core/src/main/java/io/atomix/core/counter/impl/DefaultAtomicCounterService.java
+++ core/src/main/java/io/atomix/core/counter/impl/DefaultAtomicCounterService.java
@@ -19,99 +19,71 @@ import io.atomix.core.counter.AtomicCounterType;
 import io.atomix.primitive.service.AbstractPrimitiveService;
 import io.atomix.primitive.service.BackupInput;
 import io.atomix.primitive.service.BackupOutput;
-import io.atomix.utils.serializer.Namespace;
-import io.atomix.utils.serializer.Namespaces;
-import io.atomix.utils.serializer.Serializer;
 
-import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Atomix long state.
  */
 public class DefaultAtomicCounterService extends AbstractPrimitiveService implements AtomicCounterService {
-  private static final Serializer SERIALIZER = Serializer.using(Namespace.builder()
-      .register(Namespaces.BASIC)
-      .build());
-
-  private long value;
+  private final AtomicLong counter = new AtomicLong();
 
   public DefaultAtomicCounterService() {
     super(AtomicCounterType.instance());
   }
 
   @Override
-  public Serializer serializer() {
-    return SERIALIZER;
-  }
-
-  @Override
   public void backup(BackupOutput writer) {
-    writer.writeLong(value);
+    writer.writeLong(counter.get());
   }
 
   @Override
   public void restore(BackupInput reader) {
-    value = reader.readLong();
+    counter.set(reader.readLong());
   }
 
   @Override
   public void set(long value) {
-    this.value = value;
+    counter.set(value);
   }
 
   @Override
   public long get() {
-    return value;
+    return counter.get();
   }
 
   @Override
   public boolean compareAndSet(long expect, long update) {
-    if (Objects.equals(value, expect)) {
-      value = update;
-      return true;
-    }
-    return false;
+    return counter.compareAndSet(expect, update);
   }
 
   @Override
   public long incrementAndGet() {
-    Long oldValue = value;
-    value = oldValue + 1;
-    return value;
+    return counter.incrementAndGet();
   }
 
   @Override
   public long getAndIncrement() {
-    Long oldValue = value;
-    value = oldValue + 1;
-    return oldValue;
+    return counter.getAndIncrement();
   }
 
   @Override
   public long decrementAndGet() {
-    Long oldValue = value;
-    value = oldValue - 1;
-    return value;
+    return counter.decrementAndGet();
   }
 
   @Override
   public long getAndDecrement() {
-    Long oldValue = value;
-    value = oldValue - 1;
-    return oldValue;
+    return counter.getAndDecrement();
   }
 
   @Override
   public long addAndGet(long delta) {
-    Long oldValue = value;
-    value = oldValue + delta;
-    return value;
+    return counter.addAndGet(delta);
   }
 
   @Override
   public long getAndAdd(long delta) {
-    Long oldValue = value;
-    value = oldValue + delta;
-    return oldValue;
+    return counter.getAndAdd(delta);
   }
 }
