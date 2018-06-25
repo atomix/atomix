0bff26962d5493a7c80ec7c5725be6c209ae6e4b
Jordan Halterman <jordan.halterman@gmail.com>
2018-06-24 17:07:24 -0700

    Move lock/semaphore into io.atomix.core.concurrent package.

---
+++ core/src/main/java/io/atomix/core/concurrent/impl/DistributedLockResource.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2017-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.atomix.core.concurrent.impl;
+
+import io.atomix.core.concurrent.AsyncDistributedLock;
+import io.atomix.primitive.resource.PrimitiveResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Distributed lock resource.
+ */
+public class DistributedLockResource implements PrimitiveResource {
+  private static final Logger LOGGER = LoggerFactory.getLogger(DistributedLockResource.class);
+
+  private final AsyncDistributedLock lock;
+
+  public DistributedLockResource(AsyncDistributedLock lock) {
+    this.lock = lock;
+  }
+
+  @POST
+  @Path("/lock")
+  @Produces(MediaType.APPLICATION_JSON)
+  public void lock(@Suspended AsyncResponse response) {
+    lock.lock().whenComplete((result, error) -> {
+      if (error == null) {
+        response.resume(Response.ok(result.value()).build());
+      } else {
+        LOGGER.warn("{}", error);
+        response.resume(Response.serverError().build());
+      }
+    });
+  }
+
+  @DELETE
+  @Path("/lock")
+  public void unlock(@Suspended AsyncResponse response) {
+    lock.unlock().whenComplete((result, error) -> {
+      if (error == null) {
+        response.resume(Response.ok().build());
+      } else {
+        LOGGER.warn("{}", error);
+        response.resume(Response.serverError().build());
+      }
+    });
+  }
+}
