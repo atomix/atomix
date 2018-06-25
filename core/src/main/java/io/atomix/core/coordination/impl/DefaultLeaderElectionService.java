e8ca0425a21fd361157230b9cfbf19728f1d3351
Jordan Halterman <jordan.halterman@gmail.com>
2018-06-24 17:12:55 -0700

    Move leader election and work queue primitives into io.atomix.core.coordination package.

---
+++ core/src/main/java/io/atomix/core/coordination/impl/DefaultLeaderElectionService.java
@@ -0,0 +1,364 @@
+/*
+ * Copyright 2016-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.atomix.core.coordination.impl;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Lists;
+import io.atomix.core.coordination.Leader;
+import io.atomix.core.coordination.LeaderElectionType;
+import io.atomix.core.coordination.Leadership;
+import io.atomix.primitive.service.AbstractPrimitiveService;
+import io.atomix.primitive.service.BackupInput;
+import io.atomix.primitive.service.BackupOutput;
+import io.atomix.primitive.session.Session;
+import io.atomix.primitive.session.SessionId;
+import io.atomix.utils.misc.ArraySizeHashPrinter;
+import io.atomix.utils.serializer.Namespace;
+import io.atomix.utils.serializer.Serializer;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+/**
+ * State machine for {@link LeaderElectionProxy} resource.
+ */
+public class DefaultLeaderElectionService extends AbstractPrimitiveService<LeaderElectionClient> implements LeaderElectionService {
+
+  private static final Serializer SERIALIZER = Serializer.using(Namespace.builder()
+      .register(LeaderElectionType.instance().namespace())
+      .register(SessionId.class)
+      .register(Registration.class)
+      .register(new LinkedHashMap<>().keySet().getClass())
+      .build());
+
+  private Registration leader;
+  private long term;
+  private long termStartTime;
+  private List<Registration> registrations = new LinkedList<>();
+  private AtomicLong termCounter = new AtomicLong();
+  private Set<SessionId> listeners = new LinkedHashSet<>();
+
+  public DefaultLeaderElectionService() {
+    super(LeaderElectionType.instance(), LeaderElectionClient.class);
+  }
+
+  @Override
+  public Serializer serializer() {
+    return SERIALIZER;
+  }
+
+  @Override
+  public void backup(BackupOutput writer) {
+    writer.writeLong(termCounter.get());
+    writer.writeObject(leader);
+    writer.writeLong(term);
+    writer.writeLong(termStartTime);
+    writer.writeObject(registrations);
+    writer.writeObject(listeners);
+    getLogger().debug("Took state machine snapshot");
+  }
+
+  @Override
+  public void restore(BackupInput reader) {
+    termCounter.set(reader.readLong());
+    leader = reader.readObject();
+    term = reader.readLong();
+    termStartTime = reader.readLong();
+    registrations = reader.readObject();
+    listeners = reader.readObject();
+    getLogger().debug("Reinstated state machine from snapshot");
+  }
+
+  @Override
+  public void listen() {
+    listeners.add(getCurrentSession().sessionId());
+  }
+
+  @Override
+  public void unlisten() {
+    listeners.remove(getCurrentSession().sessionId());
+  }
+
+  private void notifyLeadershipChange(Leadership<byte[]> oldLeadership, Leadership<byte[]> newLeadership) {
+    listeners.forEach(id -> getSession(id).accept(client -> client.onLeadershipChange(oldLeadership, newLeadership)));
+  }
+
+  @Override
+  public Leadership<byte[]> run(byte[] id) {
+    try {
+      Leadership<byte[]> oldLeadership = leadership();
+      Registration registration = new Registration(id, getCurrentSession().sessionId().id());
+      addRegistration(registration);
+      Leadership<byte[]> newLeadership = leadership();
+
+      if (!Objects.equal(oldLeadership, newLeadership)) {
+        notifyLeadershipChange(oldLeadership, newLeadership);
+      }
+      return newLeadership;
+    } catch (Exception e) {
+      getLogger().error("State machine operation failed", e);
+      throw Throwables.propagate(e);
+    }
+  }
+
+  @Override
+  public void withdraw(byte[] id) {
+    try {
+      Leadership<byte[]> oldLeadership = leadership();
+      cleanup(id);
+      Leadership<byte[]> newLeadership = leadership();
+      if (!Objects.equal(oldLeadership, newLeadership)) {
+        notifyLeadershipChange(oldLeadership, newLeadership);
+      }
+    } catch (Exception e) {
+      getLogger().error("State machine operation failed", e);
+      throw Throwables.propagate(e);
+    }
+  }
+
+  @Override
+  public boolean anoint(byte[] id) {
+    try {
+      Leadership<byte[]> oldLeadership = leadership();
+      Registration newLeader = registrations.stream()
+          .filter(r -> Arrays.equals(r.id(), id))
+          .findFirst()
+          .orElse(null);
+      if (newLeader != null) {
+        this.leader = newLeader;
+        this.term = termCounter.incrementAndGet();
+        this.termStartTime = getWallClock().getTime().unixTimestamp();
+      }
+      Leadership<byte[]> newLeadership = leadership();
+      if (!Objects.equal(oldLeadership, newLeadership)) {
+        notifyLeadershipChange(oldLeadership, newLeadership);
+      }
+      return leader != null && Arrays.equals(id, leader.id());
+    } catch (Exception e) {
+      getLogger().error("State machine operation failed", e);
+      throw Throwables.propagate(e);
+    }
+  }
+
+  @Override
+  public boolean promote(byte[] id) {
+    try {
+      Leadership<byte[]> oldLeadership = leadership();
+      boolean containsCandidate = oldLeadership.candidates().stream()
+          .anyMatch(a -> Arrays.equals(a, id));
+      if (!containsCandidate) {
+        return false;
+      }
+      Registration registration = registrations.stream()
+          .filter(r -> Arrays.equals(r.id(), id))
+          .findFirst()
+          .orElse(null);
+      List<Registration> updatedRegistrations = Lists.newArrayList();
+      updatedRegistrations.add(registration);
+      registrations.stream()
+          .filter(r -> !Arrays.equals(r.id(), id))
+          .forEach(updatedRegistrations::add);
+      this.registrations = updatedRegistrations;
+      Leadership<byte[]> newLeadership = leadership();
+      if (!Objects.equal(oldLeadership, newLeadership)) {
+        notifyLeadershipChange(oldLeadership, newLeadership);
+      }
+      return true;
+    } catch (Exception e) {
+      getLogger().error("State machine operation failed", e);
+      throw Throwables.propagate(e);
+    }
+  }
+
+  @Override
+  public void evict(byte[] id) {
+    try {
+      Leadership<byte[]> oldLeadership = leadership();
+      Optional<Registration> registration =
+          registrations.stream().filter(r -> Arrays.equals(r.id, id)).findFirst();
+      if (registration.isPresent()) {
+        List<Registration> updatedRegistrations =
+            registrations.stream()
+                .filter(r -> !Arrays.equals(r.id(), id))
+                .collect(Collectors.toList());
+        if (Arrays.equals(leader.id(), id)) {
+          if (!updatedRegistrations.isEmpty()) {
+            this.registrations = updatedRegistrations;
+            this.leader = updatedRegistrations.get(0);
+            this.term = termCounter.incrementAndGet();
+            this.termStartTime = getWallClock().getTime().unixTimestamp();
+          } else {
+            this.registrations = updatedRegistrations;
+            this.leader = null;
+          }
+        } else {
+          this.registrations = updatedRegistrations;
+        }
+      }
+      Leadership<byte[]> newLeadership = leadership();
+      if (!Objects.equal(oldLeadership, newLeadership)) {
+        notifyLeadershipChange(oldLeadership, newLeadership);
+      }
+    } catch (Exception e) {
+      getLogger().error("State machine operation failed", e);
+      throw Throwables.propagate(e);
+    }
+  }
+
+  @Override
+  public Leadership<byte[]> getLeadership() {
+    try {
+      return leadership();
+    } catch (Exception e) {
+      getLogger().error("State machine operation failed", e);
+      throw Throwables.propagate(e);
+    }
+  }
+
+  private Leadership<byte[]> leadership() {
+    return new Leadership<>(leader(), candidates());
+  }
+
+  private void onSessionEnd(Session session) {
+    listeners.remove(session.sessionId());
+    Leadership<byte[]> oldLeadership = leadership();
+    cleanup(session);
+    Leadership<byte[]> newLeadership = leadership();
+    if (!Objects.equal(oldLeadership, newLeadership)) {
+      notifyLeadershipChange(oldLeadership, newLeadership);
+    }
+  }
+
+  private static class Registration {
+    private final byte[] id;
+    private final long sessionId;
+
+    protected Registration(byte[] id, long sessionId) {
+      this.id = id;
+      this.sessionId = sessionId;
+    }
+
+    protected byte[] id() {
+      return id;
+    }
+
+    protected long sessionId() {
+      return sessionId;
+    }
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(getClass())
+          .add("id", ArraySizeHashPrinter.of(id))
+          .add("sessionId", sessionId)
+          .toString();
+    }
+  }
+
+  protected void cleanup(byte[] id) {
+    Optional<Registration> registration =
+        registrations.stream().filter(r -> Arrays.equals(r.id(), id)).findFirst();
+    if (registration.isPresent()) {
+      List<Registration> updatedRegistrations =
+          registrations.stream()
+              .filter(r -> !Arrays.equals(r.id(), id))
+              .collect(Collectors.toList());
+      if (Arrays.equals(leader.id(), id)) {
+        if (!updatedRegistrations.isEmpty()) {
+          this.registrations = updatedRegistrations;
+          this.leader = updatedRegistrations.get(0);
+          this.term = termCounter.incrementAndGet();
+          this.termStartTime = getWallClock().getTime().unixTimestamp();
+        } else {
+          this.registrations = updatedRegistrations;
+          this.leader = null;
+        }
+      } else {
+        this.registrations = updatedRegistrations;
+      }
+    }
+  }
+
+  protected void cleanup(Session session) {
+    Optional<Registration> registration =
+        registrations.stream().filter(r -> r.sessionId() == session.sessionId().id()).findFirst();
+    if (registration.isPresent()) {
+      List<Registration> updatedRegistrations =
+          registrations.stream()
+              .filter(r -> r.sessionId() != session.sessionId().id())
+              .collect(Collectors.toList());
+      if (leader.sessionId() == session.sessionId().id()) {
+        if (!updatedRegistrations.isEmpty()) {
+          this.registrations = updatedRegistrations;
+          this.leader = updatedRegistrations.get(0);
+          this.term = termCounter.incrementAndGet();
+          this.termStartTime = getWallClock().getTime().unixTimestamp();
+        } else {
+          this.registrations = updatedRegistrations;
+          this.leader = null;
+        }
+      } else {
+        this.registrations = updatedRegistrations;
+      }
+    }
+  }
+
+  protected Leader<byte[]> leader() {
+    if (leader == null) {
+      return null;
+    } else {
+      byte[] leaderId = leader.id();
+      return new Leader<>(leaderId, term, termStartTime);
+    }
+  }
+
+  protected List<byte[]> candidates() {
+    return registrations.stream().map(registration -> registration.id()).collect(Collectors.toList());
+  }
+
+  protected void addRegistration(Registration registration) {
+    if (registrations.stream().noneMatch(r -> Arrays.equals(registration.id(), r.id()))) {
+      List<Registration> updatedRegistrations = new LinkedList<>(registrations);
+      updatedRegistrations.add(registration);
+      boolean newLeader = leader == null;
+      this.registrations = updatedRegistrations;
+      if (newLeader) {
+        this.leader = registration;
+        this.term = termCounter.incrementAndGet();
+        this.termStartTime = getWallClock().getTime().unixTimestamp();
+      }
+    }
+  }
+
+  @Override
+  public void onExpire(Session session) {
+    onSessionEnd(session);
+  }
+
+  @Override
+  public void onClose(Session session) {
+    onSessionEnd(session);
+  }
+}
---
+++ core/src/main/java/io/atomix/core/coordination/impl/LeaderElectionResource.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2017-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.atomix.core.coordination.impl;
+
+import io.atomix.core.coordination.Leadership;
+import io.atomix.core.coordination.LeadershipEvent;
+import io.atomix.core.coordination.LeadershipEventListener;
+import io.atomix.core.coordination.AsyncLeaderElection;
+import io.atomix.core.utils.EventLog;
+import io.atomix.core.utils.EventManager;
+import io.atomix.primitive.resource.PrimitiveResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Leader election resource.
+ */
+public class LeaderElectionResource implements PrimitiveResource {
+  private static final Logger LOGGER = LoggerFactory.getLogger(LeaderElectionResource.class);
+
+  private final AsyncLeaderElection<String> election;
+
+  public LeaderElectionResource(AsyncLeaderElection<String> election) {
+    this.election = election;
+  }
+
+  /**
+   * Returns an event log name for the given identifier.
+   */
+  private String getEventLogName(String id) {
+    return String.format("%s-%s", election.name(), id);
+  }
+
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  public void run(@Context EventManager events, @Suspended AsyncResponse response) {
+    String id = UUID.randomUUID().toString();
+    EventLog<LeadershipEventListener<String>, LeadershipEvent<String>> eventLog = events.getOrCreateEventLog(
+        AsyncLeaderElection.class, getEventLogName(id), l -> e -> l.addEvent(e));
+
+    election.addListener(eventLog.listener()).whenComplete((listenResult, listenError) -> {
+      if (listenError == null) {
+        election.run(id).whenComplete((runResult, runError) -> {
+          if (runError == null) {
+            response.resume(Response.ok(id).build());
+          } else {
+            LOGGER.warn("{}", runError);
+            response.resume(Response.serverError().build());
+          }
+        });
+      } else {
+        LOGGER.warn("{}", listenError);
+        response.resume(Response.serverError().build());
+      }
+    });
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public void getLeadership(@Suspended AsyncResponse response) {
+    election.getLeadership().whenComplete((result, error) -> {
+      if (error == null) {
+        response.resume(Response.ok(new LeadershipResponse(result)).build());
+      } else {
+        LOGGER.warn("{}", error);
+        response.resume(Response.serverError().build());
+      }
+    });
+  }
+
+  @GET
+  @Path("/{id}")
+  public void listen(@PathParam("id") String id, @Context EventManager events, @Suspended AsyncResponse response) {
+    EventLog<LeadershipEventListener<String>, LeadershipEvent<String>> eventLog = events.getEventLog(
+        AsyncLeaderElection.class, getEventLogName(id));
+    consumeNextEvent(eventLog, id, response);
+  }
+
+  /**
+   * Recursively consumes events from the given event log until the next event for the given ID is located.
+   */
+  private void consumeNextEvent(EventLog<LeadershipEventListener<String>, LeadershipEvent<String>> eventLog, String id, AsyncResponse response) {
+    eventLog.nextEvent().whenComplete((event, error) -> {
+      if (error == null) {
+        if (event.newLeadership().leader() != null && event.newLeadership().leader().id().equals(id)) {
+          response.resume(Response.ok(new LeadershipResponse(event.newLeadership())).build());
+        } else if (event.newLeadership().candidates().stream().noneMatch(c -> c.equals(id))) {
+          election.removeListener(eventLog.listener()).whenComplete((removeResult, removeError) -> {
+            response.resume(Response.status(Status.NOT_FOUND).build());
+          });
+        }
+      } else {
+        response.resume(Response.status(Status.NOT_FOUND).build());
+      }
+    });
+  }
+
+  @DELETE
+  @Path("/{id}")
+  public void withdraw(@PathParam("id") String id, @Context EventManager events, @Suspended AsyncResponse response) {
+    EventLog<LeadershipEventListener<String>, LeadershipEvent<String>> eventLog = events.removeEventLog(
+        AsyncLeaderElection.class, getEventLogName(id));
+    if (eventLog != null && eventLog.close()) {
+      election.removeListener(eventLog.listener()).whenComplete((removeResult, removeError) -> {
+        election.withdraw(id).whenComplete((withdrawResult, withdrawError) -> {
+          if (withdrawError == null) {
+            response.resume(Response.ok().build());
+          } else {
+            LOGGER.warn("{}", removeError);
+            response.resume(Response.serverError().build());
+          }
+        });
+      });
+    } else {
+      response.resume(Response.ok().build());
+    }
+  }
+
+  @POST
+  @Path("/{id}/anoint")
+  public void anoint(@PathParam("id") String id, @Suspended AsyncResponse response) {
+    election.anoint(id).whenComplete((result, error) -> {
+      if (error == null) {
+        response.resume(Response.ok().build());
+      } else {
+        LOGGER.warn("{}", error);
+        response.resume(Response.serverError().build());
+      }
+    });
+  }
+
+  @POST
+  @Path("/{id}/promote")
+  public void promote(@PathParam("id") String id, @Suspended AsyncResponse response) {
+    election.promote(id).whenComplete((result, error) -> {
+      if (error == null) {
+        response.resume(Response.ok().build());
+      } else {
+        LOGGER.warn("{}", error);
+        response.resume(Response.serverError().build());
+      }
+    });
+  }
+
+  @POST
+  @Path("/{id}/evict")
+  public void evict(@PathParam("id") String id, @Suspended AsyncResponse response) {
+    election.evict(id).whenComplete((result, error) -> {
+      if (error == null) {
+        response.resume(Response.ok().build());
+      } else {
+        LOGGER.warn("{}", error);
+        response.resume(Response.serverError().build());
+      }
+    });
+  }
+
+  /**
+   * Leadership response
+   */
+  static class LeadershipResponse {
+    private final Leadership<String> leadership;
+
+    public LeadershipResponse(Leadership<String> leadership) {
+      this.leadership = leadership;
+    }
+
+    public String getLeader() {
+      return leadership.leader() != null ? leadership.leader().id() : null;
+    }
+
+    public List<String> getCandidates() {
+      return leadership.candidates();
+    }
+  }
+}
