ef38074a68b06cf2680d319907d22aad607fba60
Jordan Halterman <jordan.halterman@gmail.com>
2018-07-05 17:38:10 -0700

    Move atomic tree map tests into map package.

--- core/src/test/java/io/atomix/core/map/PrimaryBackupAtomicTreeMapTest.java
+++ core/src/test/java/io/atomix/core/map/PrimaryBackupAtomicTreeMapTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.atomix.core.treemap;
+package io.atomix.core.map;
 
 import io.atomix.primitive.protocol.PrimitiveProtocol;
 import io.atomix.protocols.backup.MultiPrimaryProtocol;
