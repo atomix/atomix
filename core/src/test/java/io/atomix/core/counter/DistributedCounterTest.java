464a4e0a219637e001adebcd1088ddcd283164fa
Jordan Halterman <jordan.halterman@gmail.com>
2018-07-11 14:23:17 -0700

    Fix broken CRDT counter tests.

--- core/src/test/java/io/atomix/core/counter/DistributedCounterTest.java
+++ core/src/test/java/io/atomix/core/counter/DistributedCounterTest.java
@@ -38,23 +38,43 @@ public abstract class DistributedCounterTest extends AbstractPrimitiveTest {
     assertEquals(2, counter1.incrementAndGet());
     assertEquals(2, counter1.get());
 
-    assertTrue(waitFor(counter2, 2, 5000));
+    assertTrue(waitFor(counter2, 2));
     assertEquals(3, counter2.incrementAndGet());
 
-    assertTrue(waitFor(counter1, 3, 5000));
+    assertTrue(waitFor(counter1, 3));
     assertEquals(4, counter1.incrementAndGet());
     assertEquals(4, counter2.incrementAndGet());
 
-    assertTrue(waitFor(counter1, 5, 5000));
-    assertTrue(waitFor(counter2, 5, 5000));
+    assertTrue(waitFor(counter1, 5));
+    assertTrue(waitFor(counter2, 5));
+
+    assertEquals(4, counter1.decrementAndGet());
+    assertEquals(4, counter2.decrementAndGet());
+
+    assertTrue(waitFor(counter1, 3));
+    assertTrue(waitFor(counter2, 3));
+
+    assertEquals(10, counter1.addAndGet(7));
+
+    assertTrue(waitFor(counter1, 10));
+    assertTrue(waitFor(counter2, 10));
+
+    assertEquals(10, counter1.getAndIncrement());
+    assertEquals(10, counter2.getAndDecrement());
+
+    assertEquals(11, counter1.get());
+    assertEquals(9, counter2.get());
+
+    assertTrue(waitFor(counter1, 10));
+    assertTrue(waitFor(counter2, 10));
   }
 
-  private boolean waitFor(DistributedCounter counter, long value, long timeout) throws InterruptedException {
+  private boolean waitFor(DistributedCounter counter, long value) throws InterruptedException {
     long startTime = System.currentTimeMillis();
     while (true) {
       if (counter.get() == value) {
         return true;
-      } else if (System.currentTimeMillis() - startTime > timeout) {
+      } else if (System.currentTimeMillis() - startTime > 5000) {
         return false;
       } else {
         Thread.sleep(10);
