839d487cbe2ae32fbe5971f9f90bb1ef7719221e
Jordan Halterman <jordan.halterman@gmail.com>
2018-07-18 01:00:20 -0700

    Fix ClassCastException in primitive getters.

--- core/src/test/java/io/atomix/core/AtomixTest.java
+++ core/src/test/java/io/atomix/core/AtomixTest.java
@@ -18,7 +18,36 @@ package io.atomix.core;
 import io.atomix.cluster.ClusterMembershipEvent;
 import io.atomix.cluster.ClusterMembershipEventListener;
 import io.atomix.cluster.Member;
+import io.atomix.core.barrier.DistributedCyclicBarrierType;
+import io.atomix.core.counter.AtomicCounterType;
+import io.atomix.core.counter.DistributedCounterType;
+import io.atomix.core.election.LeaderElectionType;
+import io.atomix.core.election.LeaderElectorType;
+import io.atomix.core.idgenerator.AtomicIdGeneratorType;
+import io.atomix.core.list.DistributedListType;
+import io.atomix.core.lock.AtomicLockType;
+import io.atomix.core.lock.DistributedLockType;
+import io.atomix.core.map.AtomicCounterMapType;
+import io.atomix.core.map.AtomicMapType;
+import io.atomix.core.map.AtomicNavigableMapType;
+import io.atomix.core.map.AtomicSortedMapType;
+import io.atomix.core.map.DistributedMapType;
+import io.atomix.core.map.DistributedNavigableMapType;
+import io.atomix.core.map.DistributedSortedMapType;
+import io.atomix.core.multimap.AtomicMultimapType;
+import io.atomix.core.multimap.DistributedMultimapType;
+import io.atomix.core.multiset.DistributedMultisetType;
 import io.atomix.core.profile.Profile;
+import io.atomix.core.queue.DistributedQueueType;
+import io.atomix.core.semaphore.AtomicSemaphoreType;
+import io.atomix.core.semaphore.DistributedSemaphoreType;
+import io.atomix.core.set.DistributedNavigableSetType;
+import io.atomix.core.set.DistributedSetType;
+import io.atomix.core.set.DistributedSortedSetType;
+import io.atomix.core.tree.AtomicDocumentTreeType;
+import io.atomix.core.value.AtomicValueType;
+import io.atomix.core.value.DistributedValueType;
+import io.atomix.core.workqueue.WorkQueueType;
 import io.atomix.utils.concurrent.Futures;
 import io.atomix.utils.net.Address;
 import org.junit.After;
@@ -40,6 +69,7 @@ import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -296,6 +326,133 @@ public class AtomixTest extends AbstractAtomixTest {
     assertNotNull(member.properties());
     assertEquals(1, member.properties().size());
     assertEquals("a-value", member.properties().get("a-key"));
+  }
+
+  @Test
+  public void testPrimitiveGetters() throws Exception {
+    List<CompletableFuture<Atomix>> futures = new ArrayList<>();
+    futures.add(startAtomix(1, Arrays.asList(1, 2, 3), Profile.consensus("1", "2", "3")));
+    futures.add(startAtomix(2, Arrays.asList(1, 2, 3), Profile.consensus("1", "2", "3")));
+    futures.add(startAtomix(3, Arrays.asList(1, 2, 3), Profile.consensus("1", "2", "3")));
+    Futures.allOf(futures).get(30, TimeUnit.SECONDS);
+
+    Atomix atomix = startAtomix(4, Arrays.asList(1, 2, 3), Profile.client()).get(30, TimeUnit.SECONDS);
+
+    assertEquals("a", atomix.getAtomicCounter("a").name());
+    assertEquals(AtomicCounterType.instance(), atomix.getAtomicCounter("a").type());
+    assertSame(atomix.getAtomicCounter("a"), atomix.getAtomicCounter("a"));
+
+    assertEquals("b", atomix.getAtomicMap("b").name());
+    assertEquals(AtomicMapType.instance(), atomix.getAtomicMap("b").type());
+    assertSame(atomix.getAtomicMap("b"), atomix.getAtomicMap("b"));
+
+    assertEquals("c", atomix.getAtomicCounterMap("c").name());
+    assertEquals(AtomicCounterMapType.instance(), atomix.getAtomicCounterMap("c").type());
+    assertSame(atomix.getAtomicCounterMap("c"), atomix.getAtomicCounterMap("c"));
+
+    assertEquals("d", atomix.getAtomicDocumentTree("d").name());
+    assertEquals(AtomicDocumentTreeType.instance(), atomix.getAtomicDocumentTree("d").type());
+    assertSame(atomix.getAtomicDocumentTree("d"), atomix.getAtomicDocumentTree("d"));
+
+    assertEquals("e", atomix.getAtomicIdGenerator("e").name());
+    assertEquals(AtomicIdGeneratorType.instance(), atomix.getAtomicIdGenerator("e").type());
+    assertSame(atomix.getAtomicIdGenerator("e"), atomix.getAtomicIdGenerator("e"));
+
+    assertEquals("f", atomix.getAtomicLock("f").name());
+    assertEquals(AtomicLockType.instance(), atomix.getAtomicLock("f").type());
+    assertSame(atomix.getAtomicLock("f"), atomix.getAtomicLock("f"));
+
+    assertEquals("g", atomix.getAtomicMultimap("g").name());
+    assertEquals(AtomicMultimapType.instance(), atomix.getAtomicMultimap("g").type());
+    assertSame(atomix.getAtomicMultimap("g"), atomix.getAtomicMultimap("g"));
+
+    assertEquals("h", atomix.getAtomicNavigableMap("h").name());
+    assertEquals(AtomicNavigableMapType.instance(), atomix.getAtomicNavigableMap("h").type());
+    assertSame(atomix.getAtomicNavigableMap("h"), atomix.getAtomicNavigableMap("h"));
+
+    assertEquals("i", atomix.getAtomicSemaphore("i").name());
+    assertEquals(AtomicSemaphoreType.instance(), atomix.getAtomicSemaphore("i").type());
+    assertSame(atomix.getAtomicSemaphore("i"), atomix.getAtomicSemaphore("i"));
+
+    assertEquals("j", atomix.getAtomicSortedMap("j").name());
+    assertEquals(AtomicSortedMapType.instance(), atomix.getAtomicSortedMap("j").type());
+    assertSame(atomix.getAtomicSortedMap("j"), atomix.getAtomicSortedMap("j"));
+
+    assertEquals("k", atomix.getAtomicValue("k").name());
+    assertEquals(AtomicValueType.instance(), atomix.getAtomicValue("k").type());
+    assertSame(atomix.getAtomicValue("k"), atomix.getAtomicValue("k"));
+
+    assertEquals("l", atomix.getCounter("l").name());
+    assertEquals(DistributedCounterType.instance(), atomix.getCounter("l").type());
+    assertSame(atomix.getCounter("l"), atomix.getCounter("l"));
+
+    assertEquals("m", atomix.getCyclicBarrier("m").name());
+    assertEquals(DistributedCyclicBarrierType.instance(), atomix.getCyclicBarrier("m").type());
+    assertSame(atomix.getCyclicBarrier("m"), atomix.getCyclicBarrier("m"));
+
+    assertEquals("n", atomix.getLeaderElection("n").name());
+    assertEquals(LeaderElectionType.instance(), atomix.getLeaderElection("n").type());
+    assertSame(atomix.getLeaderElection("n"), atomix.getLeaderElection("n"));
+
+    assertEquals("o", atomix.getLeaderElector("o").name());
+    assertEquals(LeaderElectorType.instance(), atomix.getLeaderElector("o").type());
+    assertSame(atomix.getLeaderElector("o"), atomix.getLeaderElector("o"));
+
+    assertEquals("p", atomix.getList("p").name());
+    assertEquals(DistributedListType.instance(), atomix.getList("p").type());
+    assertSame(atomix.getList("p"), atomix.getList("p"));
+
+    assertEquals("q", atomix.getLock("q").name());
+    assertEquals(DistributedLockType.instance(), atomix.getLock("q").type());
+    assertSame(atomix.getLock("q"), atomix.getLock("q"));
+
+    assertEquals("r", atomix.getMap("r").name());
+    assertEquals(DistributedMapType.instance(), atomix.getMap("r").type());
+    assertSame(atomix.getMap("r"), atomix.getMap("r"));
+
+    assertEquals("s", atomix.getMultimap("s").name());
+    assertEquals(DistributedMultimapType.instance(), atomix.getMultimap("s").type());
+    assertSame(atomix.getMultimap("s"), atomix.getMultimap("s"));
+
+    assertEquals("t", atomix.getMultiset("t").name());
+    assertEquals(DistributedMultisetType.instance(), atomix.getMultiset("t").type());
+    assertSame(atomix.getMultiset("t"), atomix.getMultiset("t"));
+
+    assertEquals("u", atomix.getNavigableMap("u").name());
+    assertEquals(DistributedNavigableMapType.instance(), atomix.getNavigableMap("u").type());
+    assertSame(atomix.getNavigableMap("u"), atomix.getNavigableMap("u"));
+
+    assertEquals("v", atomix.getNavigableSet("v").name());
+    assertEquals(DistributedNavigableSetType.instance(), atomix.getNavigableSet("v").type());
+    assertSame(atomix.getNavigableSet("v"), atomix.getNavigableSet("v"));
+
+    assertEquals("w", atomix.getQueue("w").name());
+    assertEquals(DistributedQueueType.instance(), atomix.getQueue("w").type());
+    assertSame(atomix.getQueue("w"), atomix.getQueue("w"));
+
+    assertEquals("x", atomix.getSemaphore("x").name());
+    assertEquals(DistributedSemaphoreType.instance(), atomix.getSemaphore("x").type());
+    assertSame(atomix.getSemaphore("x"), atomix.getSemaphore("x"));
+
+    assertEquals("y", atomix.getSet("y").name());
+    assertEquals(DistributedSetType.instance(), atomix.getSet("y").type());
+    assertSame(atomix.getSet("y"), atomix.getSet("y"));
+
+    assertEquals("z", atomix.getSortedMap("z").name());
+    assertEquals(DistributedSortedMapType.instance(), atomix.getSortedMap("z").type());
+    assertSame(atomix.getSortedMap("z"), atomix.getSortedMap("z"));
+
+    assertEquals("aa", atomix.getSortedSet("aa").name());
+    assertEquals(DistributedSortedSetType.instance(), atomix.getSortedSet("aa").type());
+    assertSame(atomix.getSortedSet("aa"), atomix.getSortedSet("aa"));
+
+    assertEquals("bb", atomix.getValue("bb").name());
+    assertEquals(DistributedValueType.instance(), atomix.getValue("bb").type());
+    assertSame(atomix.getValue("bb"), atomix.getValue("bb"));
+
+    assertEquals("cc", atomix.getWorkQueue("cc").name());
+    assertEquals(WorkQueueType.instance(), atomix.getWorkQueue("cc").type());
+    assertSame(atomix.getWorkQueue("cc"), atomix.getWorkQueue("cc"));
   }
 
   private static class TestClusterMembershipEventListener implements ClusterMembershipEventListener {
