e31e7a0db2d0b89934d86eafe1c2f55f22e09229
Jordan Halterman <jordan.halterman@gmail.com>
2018-07-26 15:07:10 -0700

    Add support for node type serialization in document tree primitive builder.

--- core/src/main/java/io/atomix/core/tree/AtomicDocumentTreeConfig.java
+++ core/src/main/java/io/atomix/core/tree/AtomicDocumentTreeConfig.java
@@ -15,15 +15,20 @@
  */
 package io.atomix.core.tree;
 
-import io.atomix.primitive.Ordering;
-import io.atomix.primitive.config.PrimitiveConfig;
 import io.atomix.primitive.PrimitiveType;
+import io.atomix.primitive.config.PrimitiveConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Document tree configuration.
  */
 public class AtomicDocumentTreeConfig extends PrimitiveConfig<AtomicDocumentTreeConfig> {
-  private Ordering ordering;
+  private Class<?> nodeType;
+  private List<Class<?>> extraTypes = new ArrayList<>();
+  private boolean registrationRequired = false;
+  private boolean compatibleSerialization = false;
 
   @Override
   public PrimitiveType getType() {
@@ -31,25 +36,98 @@ public class AtomicDocumentTreeConfig extends PrimitiveConfig<AtomicDocumentTree
   }
 
   /**
-   * Sets the ordering of the tree nodes.
-   * <p>
-   * When {@link AsyncAtomicDocumentTree#getChildren(DocumentPath)} is called, children will be returned according to
-   * the specified sort order.
+   * Returns the node type.
    *
-   * @param ordering ordering of the tree nodes
-   * @return this builder
+   * @return the node type
    */
-  public AtomicDocumentTreeConfig setOrdering(Ordering ordering) {
-    this.ordering = ordering;
+  public Class<?> getNodeType() {
+    return nodeType;
+  }
+
+  /**
+   * Sets the node type.
+   *
+   * @param nodeType the document tree node type
+   * @return the document tree configuration
+   */
+  @SuppressWarnings("unchecked")
+  public AtomicDocumentTreeConfig setNodeType(Class<?> nodeType) {
+    this.nodeType = nodeType;
     return this;
   }
 
   /**
-   * Returns the document tree ordering.
+   * Returns the extra serializable types.
    *
-   * @return the document tree ordering
+   * @return the extra serializable types
    */
-  public Ordering getOrdering() {
-    return ordering;
+  public List<Class<?>> getExtraTypes() {
+    return extraTypes;
+  }
+
+  /**
+   * Sets the extra serializable types.
+   *
+   * @param extraTypes the extra serializable types
+   * @return the document tree configuration
+   */
+  @SuppressWarnings("unchecked")
+  public AtomicDocumentTreeConfig setExtraTypes(List<Class<?>> extraTypes) {
+    this.extraTypes = extraTypes;
+    return this;
+  }
+
+  /**
+   * Adds an extra serializable type.
+   *
+   * @param extraType the extra type to add
+   * @return the document tree configuration
+   */
+  @SuppressWarnings("unchecked")
+  public AtomicDocumentTreeConfig addExtraType(Class<?> extraType) {
+    extraTypes.add(extraType);
+    return this;
+  }
+
+  /**
+   * Returns whether registration is required for serializable types.
+   *
+   * @return whether registration is required for serializable types
+   */
+  public boolean isRegistrationRequired() {
+    return registrationRequired;
+  }
+
+  /**
+   * Sets whether registration is required for serializable types.
+   *
+   * @param registrationRequired whether registration is required for serializable types
+   * @return the document tree configuration
+   */
+  @SuppressWarnings("unchecked")
+  public AtomicDocumentTreeConfig setRegistrationRequired(boolean registrationRequired) {
+    this.registrationRequired = registrationRequired;
+    return this;
+  }
+
+  /**
+   * Returns whether compatible serialization is enabled.
+   *
+   * @return whether compatible serialization is enabled
+   */
+  public boolean isCompatibleSerialization() {
+    return compatibleSerialization;
+  }
+
+  /**
+   * Sets whether compatible serialization is enabled.
+   *
+   * @param compatibleSerialization whether compatible serialization is enabled
+   * @return the document tree configuration
+   */
+  @SuppressWarnings("unchecked")
+  public AtomicDocumentTreeConfig setCompatibleSerialization(boolean compatibleSerialization) {
+    this.compatibleSerialization = compatibleSerialization;
+    return this;
   }
 }
