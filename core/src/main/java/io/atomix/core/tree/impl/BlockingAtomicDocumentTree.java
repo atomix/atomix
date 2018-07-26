d8f7d7f94e510e463b97b948c075ff97fcdc30ab
Jordan Halterman <jordan.halterman@gmail.com>
2018-07-26 10:38:30 -0700

    Use filesystem-like document paths for AtomicDocumentTree.

--- core/src/main/java/io/atomix/core/tree/impl/BlockingAtomicDocumentTree.java
+++ core/src/main/java/io/atomix/core/tree/impl/BlockingAtomicDocumentTree.java
@@ -18,9 +18,9 @@ package io.atomix.core.tree.impl;
 
 import com.google.common.base.Throwables;
 import io.atomix.core.tree.AsyncAtomicDocumentTree;
+import io.atomix.core.tree.AtomicDocumentTree;
 import io.atomix.core.tree.DocumentException;
 import io.atomix.core.tree.DocumentPath;
-import io.atomix.core.tree.AtomicDocumentTree;
 import io.atomix.core.tree.DocumentTreeEventListener;
 import io.atomix.primitive.PrimitiveException;
 import io.atomix.primitive.Synchronous;
@@ -29,6 +29,7 @@ import io.atomix.utils.time.Versioned;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -96,18 +97,13 @@ public class BlockingAtomicDocumentTree<V> extends Synchronous<AsyncAtomicDocume
   }
 
   @Override
-  public void addListener(DocumentPath path, DocumentTreeEventListener<V> listener) {
-    complete(backingTree.addListener(path, listener));
+  public void addListener(DocumentPath path, DocumentTreeEventListener<V> listener, Executor executor) {
+    complete(backingTree.addListener(path, listener, executor));
   }
 
   @Override
   public void removeListener(DocumentTreeEventListener<V> listener) {
     complete(backingTree.removeListener(listener));
-  }
-
-  @Override
-  public void addListener(DocumentTreeEventListener<V> listener) {
-    complete(backingTree.addListener(listener));
   }
 
   @Override
---
+++ core/src/test/java/io/atomix/core/tree/DocumentPathTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.atomix.core.tree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Document path test.
+ */
+public class DocumentPathTest {
+  @Test
+  public void testValidDocumentPath() {
+    assertEquals("/", DocumentPath.from("/").toString());
+    assertEquals(1, DocumentPath.from("/").pathElements().size());
+    assertEquals("/foo", DocumentPath.from("/foo").toString());
+    assertEquals(2, DocumentPath.from("/foo").pathElements().size());
+    assertEquals("/foo/bar", DocumentPath.from("/foo/bar").toString());
+    assertEquals(3, DocumentPath.from("/foo/bar").pathElements().size());
+    assertEquals("foo", DocumentPath.from("/foo").childPath().toString());
+    assertEquals("/", DocumentPath.from("/foo").parent().toString());
+    assertEquals("bar", DocumentPath.from("/foo/bar").childPath().toString());
+    assertEquals("/foo", DocumentPath.from("/foo/bar").parent().toString());
+    assertEquals("foo", DocumentPath.from("foo").toString());
+    assertEquals("foo/bar", DocumentPath.from("foo/bar").toString());
+    assertEquals(1, DocumentPath.from("foo").pathElements().size());
+    assertEquals(2, DocumentPath.from("foo/bar").pathElements().size());
+  }
+}
