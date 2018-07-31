7bdfbb5d6be3a76817bdbdbbb7044d05b38005b2
Johno Crawford <johno.crawford@gmail.com>
2018-07-31 10:15:12 +0200

    Allow failure for openjdk10

--- bin/ci-push-javadoc.sh
+++ bin/ci-push-javadoc.sh
@@ -4,7 +4,7 @@
 PROJECT=atomix
 
 if [ "$TRAVIS_REPO_SLUG" == "atomix/$PROJECT" ] && \
-   [ "$TRAVIS_JDK_VERSION" == "oraclejdk10" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Publishing Javadoc...\n"
