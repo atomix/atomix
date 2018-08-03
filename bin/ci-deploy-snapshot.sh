aa98071016bc329a79d4ccda53327b3db8a6ac8a
Johno Crawford <johno@hellface.com>
2018-08-04 00:05:57 +0200

    Drop maven-shade-plugin (#763)
    * Drop maven-shade-plugin
    
    * Include third party license information with distribution artifact.

--- bin/ci-deploy-snapshot.sh
+++ bin/ci-deploy-snapshot.sh
@@ -8,7 +8,7 @@ if [ "$TRAVIS_REPO_SLUG" == "$REPO" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Publishing maven snapshot...\n"
 
-  $TRAVIS_BUILD_DIR/mvnw clean source:jar deploy --batch-mode --settings="bin/settings.xml" -Ddockerfile.username=$DOCKER_USERNAME -Ddockerfile.password=$DOCKER_PASSWORD -Dmaven.test.skip=true -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+  mvn clean source:jar deploy --batch-mode --settings="bin/settings.xml" -Ddockerfile.username=$DOCKER_USERNAME -Ddockerfile.password=$DOCKER_PASSWORD -Dmaven.test.skip=true -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 
   echo -e "Published maven snapshot"
 fi
