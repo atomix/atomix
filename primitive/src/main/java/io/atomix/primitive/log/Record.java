12cd4246f1be15672ca947d0f93c42ecb256122a
Jordan Halterman <jordan.halterman@gmail.com>
2018-10-30 18:33:05 -0700

    Add log abstractions for distributed primitives.

---
+++ primitive/src/main/java/io/atomix/primitive/log/Record.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.atomix.primitive.log;
+
+import io.atomix.utils.misc.ArraySizeHashPrinter;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+
+/**
+ * Log entry.
+ */
+public class Record {
+  private final long offset;
+  private final long timestamp;
+  private final byte[] value;
+
+  public Record(long offset, long timestamp, byte[] value) {
+    this.offset = offset;
+    this.timestamp = timestamp;
+    this.value = value;
+  }
+
+  public long offset() {
+    return offset;
+  }
+
+  public long timestamp() {
+    return timestamp;
+  }
+
+  public byte[] value() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return toStringHelper(this)
+        .add("offset", offset())
+        .add("timestamp", timestamp())
+        .add("value", ArraySizeHashPrinter.of(value()))
+        .toString();
+  }
+}
