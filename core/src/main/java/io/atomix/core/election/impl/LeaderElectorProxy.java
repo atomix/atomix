f6cbb8c6ef35ff742b741c982aaafb7e652b976d
Jordan Halterman <jordan.halterman@gmail.com>
2019-01-03 16:43:01 -0800

    Fix improper condition for adding listeners to LeaderElector.

--- core/src/main/java/io/atomix/core/election/impl/LeaderElectorProxy.java
+++ core/src/main/java/io/atomix/core/election/impl/LeaderElectorProxy.java
@@ -141,7 +141,7 @@ public class LeaderElectorProxy
   }
 
   private boolean isListening() {
-    return !leadershipChangeListeners.isEmpty() && !topicListeners.isEmpty();
+    return !leadershipChangeListeners.isEmpty() || !topicListeners.isEmpty();
   }
 
   @Override
