19bd4940b82ec5f7d3ba539052ee333f35665464
Philipp Ossler <philipp.ossler@gmail.com>
2019-12-23 10:25:40 +0100

    fix(protocols/raft): apply raft partition group configuration (#1054)
    * fix(protocols/raft): apply raft partition group configuration
    
    * apply configuration instead of using hardcoded values for election timeout, heartbeat interval and session timeout
    
    * install-jdk.sh no longer supports oraclejdk8.
    
    Co-authored-by: Johno Crawford <johno@hellface.com>

--- protocols/raft/src/main/java/io/atomix/protocols/raft/partition/impl/RaftPartitionServer.java
+++ protocols/raft/src/main/java/io/atomix/protocols/raft/partition/impl/RaftPartitionServer.java
@@ -37,7 +37,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.time.Duration;
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
@@ -49,9 +48,6 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class RaftPartitionServer implements Managed<RaftPartitionServer> {
 
   private final Logger log = getLogger(getClass());
-
-  private static final long ELECTION_TIMEOUT_MILLIS = 2500;
-  private static final long HEARTBEAT_INTERVAL_MILLIS = 250;
 
   private final MemberId localMemberId;
   private final RaftPartition partition;
@@ -162,8 +158,9 @@ public class RaftPartitionServer implements Managed<RaftPartitionServer> {
             Serializer.using(RaftNamespaces.RAFT_PROTOCOL),
             clusterCommunicator))
         .withPrimitiveTypes(primitiveTypes)
-        .withElectionTimeout(Duration.ofMillis(ELECTION_TIMEOUT_MILLIS))
-        .withHeartbeatInterval(Duration.ofMillis(HEARTBEAT_INTERVAL_MILLIS))
+        .withElectionTimeout(config.getElectionTimeout())
+        .withHeartbeatInterval(config.getHeartbeatInterval())
+        .withSessionTimeout(config.getDefaultSessionTimeout())
         .withStorage(RaftStorage.builder()
             .withPrefix(partition.name())
             .withDirectory(partition.dataDirectory())
