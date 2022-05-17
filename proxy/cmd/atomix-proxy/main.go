1cd987bb317057eec373be3c8ee7e64548d5a53d
Jordan Halterman <jordan.halterman@intel.com>
2022-05-17 02:31:53 -0700

    Add proxy submodule

--- proxy/cmd/atomix-proxy/main.go
+++ proxy/cmd/atomix-proxy/main.go
@@ -1,9 +1,85 @@
-// SPDX-FileCopyrightText: 2022-present Open Networking Foundation <info@opennetworking.org>
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
 //
 // SPDX-License-Identifier: Apache-2.0
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/atomix/runtime/proxy/pkg/runtime"
+	"github.com/spf13/cobra"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
+func main() {
+	cmd := &cobra.Command{
+		Use: "atomix-proxy",
+		Run: func(cmd *cobra.Command, args []string) {
+			proxyHost, err := cmd.Flags().GetString("proxy-host")
+			if err != nil {
+				fmt.Fprintln(cmd.OutOrStderr(), err.Error())
+				os.Exit(1)
+			}
+			proxyPort, err := cmd.Flags().GetInt("proxy-port")
+			if err != nil {
+				fmt.Fprintln(cmd.OutOrStderr(), err.Error())
+				os.Exit(1)
+			}
+			runtimeHost, err := cmd.Flags().GetString("runtime-host")
+			if err != nil {
+				fmt.Fprintln(cmd.OutOrStderr(), err.Error())
+				os.Exit(1)
+			}
+			runtimePort, err := cmd.Flags().GetInt("runtime-port")
+			if err != nil {
+				fmt.Fprintln(cmd.OutOrStderr(), err.Error())
+				os.Exit(1)
+			}
+			configFile, err := cmd.Flags().GetString("config")
+			if err != nil {
+				fmt.Fprintln(cmd.OutOrStderr(), err.Error())
+				os.Exit(1)
+			}
+			cacheDir, err := cmd.Flags().GetString("cache")
+			if err != nil {
+				fmt.Fprintln(cmd.OutOrStderr(), err.Error())
+				os.Exit(1)
+			}
+
+			rt := runtime.New(
+				runtime.WithProxyHost(proxyHost),
+				runtime.WithProxyPort(proxyPort),
+				runtime.WithRuntimeHost(runtimeHost),
+				runtime.WithRuntimePort(runtimePort),
+				runtime.WithConfigFile(configFile),
+				runtime.WithCacheDir(cacheDir))
+			if err := rt.Start(); err != nil {
+				fmt.Fprintln(cmd.OutOrStderr(), err.Error())
+				os.Exit(1)
+			}
+
+			// Wait for an interrupt signal
+			ch := make(chan os.Signal, 2)
+			signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+			<-ch
+
+			// Stop the proxy service
+			if err := rt.Stop(); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		},
+	}
+	cmd.Flags().StringP("proxy-host", "h", "", "the host to which to bind the proxy server")
+	cmd.Flags().IntP("proxy-port", "p", 5678, "the port to which to bind the proxy server")
+	cmd.Flags().String("runtime-host", "", "the host to which to bind the runtime server")
+	cmd.Flags().Int("runtime-port", 5679, "the port to which to bind the runtime server")
+	cmd.Flags().StringP("config", "c", "~/.atomix/config.yaml", "the path to the Atomix configuration file")
+	cmd.Flags().String("cache", "~/.atomix/cache", "the path to the Atomix cache")
+
+	if err := cmd.Execute(); err != nil {
+		panic(err)
+	}
 }
---
+++ proxy/pkg/runtime/server.go
@@ -0,0 +1,155 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package runtime
+
+import (
+	"context"
+	runtimev1 "github.com/atomix/runtime/api/atomix/runtime/v1"
+	"github.com/atomix/runtime/pkg/errors"
+	"github.com/atomix/runtime/pkg/logging"
+	"io"
+	"os"
+)
+
+func newRuntimeServer(runtime *Runtime) runtimev1.RuntimeServer {
+	return &runtimeServer{
+		runtime: runtime,
+	}
+}
+
+type runtimeServer struct {
+	runtime *Runtime
+}
+
+func (s *runtimeServer) ConnectCluster(ctx context.Context, request *runtimev1.ConnectClusterRequest) (*runtimev1.ConnectClusterResponse, error) {
+	log.Debugw("ConnectCluster",
+		logging.Stringer("ConnectClusterRequest", request))
+	err := s.runtime.addConnection(request.Cluster, request.Connection)
+	if err != nil {
+		log.Errorw("ConnectCluster",
+			logging.Stringer("ConnectClusterRequest", request),
+			logging.Error("Error", err))
+		return nil, errors.ToProto(err)
+	}
+	response := &runtimev1.ConnectClusterResponse{}
+	log.Debugw("ConnectCluster",
+		logging.Stringer("ConnectClusterResponse", response))
+	return response, nil
+}
+
+func (s *runtimeServer) ConfigureCluster(ctx context.Context, request *runtimev1.ConfigureClusterRequest) (*runtimev1.ConfigureClusterResponse, error) {
+	log.Debugw("ConfigureCluster",
+		logging.Stringer("ConfigureClusterRequest", request))
+	err := s.runtime.configureConnection(ctx, request.Cluster, request.Connection)
+	if err != nil {
+		log.Errorw("ConfigureCluster",
+			logging.Stringer("ConfigureClusterRequest", request),
+			logging.Error("Error", err))
+		return nil, errors.ToProto(err)
+	}
+	response := &runtimev1.ConfigureClusterResponse{}
+	log.Debugw("ConfigureCluster",
+		logging.Stringer("ConfigureClusterResponse", response))
+	return response, nil
+}
+
+func (s *runtimeServer) DisconnectCluster(ctx context.Context, request *runtimev1.DisconnectClusterRequest) (*runtimev1.DisconnectClusterResponse, error) {
+	log.Debugw("DisconnectCluster",
+		logging.Stringer("DisconnectClusterRequest", request))
+	err := s.runtime.closeConnection(ctx, request.Cluster)
+	if err != nil {
+		log.Errorw("DisconnectCluster",
+			logging.Stringer("DisconnectClusterRequest", request),
+			logging.Error("Error", err))
+		return nil, errors.ToProto(err)
+	}
+	response := &runtimev1.DisconnectClusterResponse{}
+	log.Debugw("DisconnectCluster",
+		logging.Stringer("DisconnectClusterResponse", response))
+	return response, nil
+}
+
+func (s *runtimeServer) InstallDriver(server runtimev1.Runtime_InstallDriverServer) error {
+	initRequest, err := server.Recv()
+	if err != nil {
+		log.Errorw("InstallDriver",
+			logging.Stringer("InstallDriverRequest", initRequest),
+			logging.Error("Error", err))
+		return err
+	}
+
+	log.Debugw("InstallDriver",
+		logging.Stringer("InstallDriverRequest", initRequest))
+
+	var plugin *DriverPlugin
+	switch r := initRequest.Driver.(type) {
+	case *runtimev1.InstallDriverRequest_Header:
+		plugin = s.runtime.drivers.Get(r.Header.Driver.Name, r.Header.Driver.Version)
+	default:
+		return errors.ToProto(errors.NewInvalid("expected header request"))
+	}
+
+	writer, err := plugin.Create()
+	if err != nil {
+		if !os.IsExist(err) {
+			err := errors.NewInternal(err.Error())
+			log.Errorw("InstallDriver",
+				logging.Stringer("InstallDriverRequest", initRequest),
+				logging.Error("Error", err))
+			return errors.ToProto(err)
+		}
+		response := &runtimev1.InstallDriverResponse{}
+		log.Debugw("InstallDriver",
+			logging.Stringer("InstallDriverResponse", response))
+		return server.SendAndClose(response)
+	}
+
+	for {
+		request, err := server.Recv()
+		if err == io.EOF {
+			err := errors.NewInvalid("expected trailer request")
+			log.Errorw("InstallDriver",
+				logging.Stringer("InstallDriverRequest", initRequest),
+				logging.Error("Error", err))
+			return errors.ToProto(err)
+		}
+		if err != nil {
+			log.Errorw("InstallDriver",
+				logging.Stringer("InstallDriverRequest", initRequest),
+				logging.Error("Error", err))
+			return err
+		}
+
+		log.Debugw("InstallDriver",
+			logging.Stringer("InstallDriverRequest", request))
+
+		switch r := request.Driver.(type) {
+		case *runtimev1.InstallDriverRequest_Chunk:
+			if i, err := writer.Write(r.Chunk.Data); err != nil {
+				return errors.ToProto(errors.NewFault(err.Error()))
+			} else {
+				log.Debugf("Wrote %d bytes", i)
+			}
+		case *runtimev1.InstallDriverRequest_Trailer:
+			log.Debugw("InstallDriver",
+				logging.Stringer("InstallDriverRequest", request))
+			if err := writer.Close(r.Trailer.Checksum); err != nil {
+				return errors.ToProto(err)
+			}
+			response := &runtimev1.InstallDriverResponse{}
+			log.Debugw("InstallDriver",
+				logging.Stringer("InstallDriverResponse", response))
+			return server.SendAndClose(response)
+		default:
+			err := errors.NewInvalid("expected chunk or trailer request")
+			log.Errorw("InstallDriver",
+				logging.Stringer("InstallDriverRequest", initRequest),
+				logging.Error("Error", err))
+			return errors.ToProto(err)
+		}
+	}
+}
+
+var _ runtimev1.RuntimeServer = (*runtimeServer)(nil)
