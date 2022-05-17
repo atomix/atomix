4e0a525ca6abbfc8bf535d468c2c17f3ae3d4008
Jordan Halterman <jordan.halterman@intel.com>
2022-05-17 16:01:00 -0700

    Add YAML codec

---
+++ pkg/config/yaml.go
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2022-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package config
+
+import (
+	"gopkg.in/yaml.v3"
+)
+
+func NewYAMLCodec[C struct{}]() Codec[C] {
+	return &yamlCodec[C]{}
+}
+
+type yamlCodec[C struct{}] struct{}
+
+func (c *yamlCodec[C]) Encode(config C) ([]byte, error) {
+	return yaml.Marshal(&config)
+}
+
+func (c *yamlCodec[C]) Decode(bytes []byte) (C, error) {
+	var config C
+	if err := yaml.Unmarshal(bytes, &config); err != nil {
+		return config, err
+	}
+	return config, nil
+}
+
+var _ Codec[struct{}] = (*yamlCodec[struct{}])(nil)
