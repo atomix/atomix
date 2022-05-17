4907087c60982edacca9dbb55e873bb275d99c21
Jordan Halterman <jordan.halterman@intel.com>
2022-05-17 02:40:13 -0700

    Add missing API files

---
+++ api/atomix/runtime/v1/timestamp.go
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1
+
+import "time"
+
+type PhysicalTime = time.Time
+
+type LogicalTime uint64
+
+type Epoch uint64
