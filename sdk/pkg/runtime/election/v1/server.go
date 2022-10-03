b40c1c49fdcd939ef1af657068840786c75966f9
Jordan Halterman <jordan.halterman@intel.com>
2022-10-02 22:55:50 -0700

    Update API to v0.5.5

--- sdk/pkg/runtime/election/v1/server.go
+++ sdk/pkg/runtime/election/v1/server.go
@@ -162,6 +162,29 @@ func (s *leaderElectionServer) Promote(ctx context.Context, request *electionv1.
 	return response, nil
 }
 
+func (s *leaderElectionServer) Demote(ctx context.Context, request *electionv1.DemoteRequest) (*electionv1.DemoteResponse, error) {
+	log.Debugw("Demote",
+		logging.Stringer("DemoteRequest", request))
+	client, err := s.delegate.Get(request.ID.Name)
+	if err != nil {
+		err = errors.ToProto(err)
+		log.Warnw("Demote",
+			logging.Stringer("DemoteRequest", request),
+			logging.Error("Error", err))
+		return nil, err
+	}
+	response, err := client.Demote(ctx, request)
+	if err != nil {
+		log.Warnw("Demote",
+			logging.Stringer("DemoteRequest", request),
+			logging.Error("Error", err))
+		return nil, err
+	}
+	log.Debugw("Demote",
+		logging.Stringer("DemoteResponse", response))
+	return response, nil
+}
+
 func (s *leaderElectionServer) Evict(ctx context.Context, request *electionv1.EvictRequest) (*electionv1.EvictResponse, error) {
 	log.Debugw("Evict",
 		logging.Stringer("EvictRequest", request))
