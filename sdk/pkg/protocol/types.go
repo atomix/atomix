c0c0889b3c971193b6e4fc0ae34233001751588b
Jordan Halterman <jordan.halterman@intel.com>
2022-10-18 18:14:25 -0700

    Add 'protocol' package with Protobuf API for protocol implementation

---
+++ sdk/pkg/protocol/types.go
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package protocol
+
+type PartitionID uint32
+
+type MemberID uint32
+
+type GroupID uint32
+
+type SessionID uint64
+
+type PrimitiveID uint64
+
+type SequenceNum uint64
+
+type Term uint64
+
+type Index uint64
