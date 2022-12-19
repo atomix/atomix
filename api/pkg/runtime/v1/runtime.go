628ae01a37ac463f565a11ec85e2bc4061573c96
Jordan Halterman <jordan.halterman@intel.com>
2022-12-19 14:37:59 -0800

    Add helper function to unmarshal primitive configuration

---
+++ api/pkg/runtime/v1/runtime.go
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1
+
+import (
+	"github.com/gogo/protobuf/jsonpb"
+	"github.com/gogo/protobuf/proto"
+)
+
+func (m *PrimitiveSpec) UnmarshalConfig(config proto.Message) error {
+	return jsonpb.UnmarshalString(string(m.Config), config)
+}
