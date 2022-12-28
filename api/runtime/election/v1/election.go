3143c3c4ae19303cea1db4faf80fc9a7a8291725
Jordan Halterman <jordan.halterman@intel.com>
2022-12-28 10:44:36 -0800

    Add PrimitiveTypes to primitive APIs

---
+++ api/runtime/election/v1/election.go
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1
+
+import runtimev1 "github.com/atomix/atomix/api/runtime/v1"
+
+const (
+	Name       = "LeaderElection"
+	APIVersion = "v1"
+)
+
+var PrimitiveType = runtimev1.PrimitiveType{
+	Name:       Name,
+	APIVersion: APIVersion,
+}
