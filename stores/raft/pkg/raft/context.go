623d5c7d45a8849b8eb4bd5d1918f0bf66882c05
Jordan Halterman <jordan.halterman@intel.com>
2022-12-29 01:27:26 -0800

    Use sync.Map for stream registry in Raft state machine

--- stores/raft/pkg/raft/context.go
+++ stores/raft/pkg/raft/context.go
@@ -14,15 +14,12 @@ import (
 
 // newContext returns a new protocol context
 func newContext() *protocolContext {
-	return &protocolContext{
-		streams: make(map[protocolStreamID]streams.WriteStream[*rsmv1.ProposalOutput]),
-	}
+	return &protocolContext{}
 }
 
 // protocolContext stores state shared by servers and the state machine
 type protocolContext struct {
-	streams     map[protocolStreamID]streams.WriteStream[*rsmv1.ProposalOutput]
-	streamsMu   sync.RWMutex
+	streams     sync.Map
 	sequenceNum atomic.Uint64
 }
 
@@ -33,35 +30,20 @@ func (r *protocolContext) addStream(term raftv1.Term, stream streams.WriteStream
 		term:        term,
 		sequenceNum: sequenceNum,
 	}
-	r.streamsMu.Lock()
-	r.streams[streamID] = streams.NewCloserStream[*rsmv1.ProposalOutput](stream, func(s streams.WriteStream[*rsmv1.ProposalOutput]) {
-		r.removeStream(term, sequenceNum)
-	})
-	r.streamsMu.Unlock()
+	r.streams.Store(streamID, streams.NewCloserStream[*rsmv1.ProposalOutput](stream, func(s streams.WriteStream[*rsmv1.ProposalOutput]) {
+		r.streams.Delete(streamID)
+	}))
 	return sequenceNum
-}
-
-// removeStream removes a stream by ID
-func (r *protocolContext) removeStream(term raftv1.Term, sequenceNum raftv1.SequenceNum) {
-	r.streamsMu.Lock()
-	defer r.streamsMu.Unlock()
-	streamID := protocolStreamID{
-		term:        term,
-		sequenceNum: sequenceNum,
-	}
-	delete(r.streams, streamID)
 }
 
 // getStream gets a stream by ID
 func (r *protocolContext) getStream(term raftv1.Term, sequenceNum raftv1.SequenceNum) streams.WriteStream[*rsmv1.ProposalOutput] {
-	r.streamsMu.RLock()
-	defer r.streamsMu.RUnlock()
 	streamID := protocolStreamID{
 		term:        term,
 		sequenceNum: sequenceNum,
 	}
-	if stream, ok := r.streams[streamID]; ok {
-		return stream
+	if stream, ok := r.streams.Load(streamID); ok {
+		return stream.(streams.WriteStream[*rsmv1.ProposalOutput])
 	}
 	return streams.NewNilStream[*rsmv1.ProposalOutput]()
 }
