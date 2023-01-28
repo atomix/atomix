0b2aec5d4ab6d23d374a389f9b07a534acb644d9
Jordan Halterman <jordan.halterman@gmail.com>
2023-01-28 04:12:38 -0800

    Update Raft driver to RSM protocol v0.2.1

--- drivers/raft/v1/driver/conn.go
+++ drivers/raft/v1/driver/conn.go
@@ -6,29 +6,28 @@ package driver
 
 import (
 	"context"
-	counterruntimev1 "github.com/atomix/atomix/api/runtime/counter/v1"
-	countermapruntimev1 "github.com/atomix/atomix/api/runtime/countermap/v1"
-	electionruntimev1 "github.com/atomix/atomix/api/runtime/election/v1"
-	indexedmapruntimev1 "github.com/atomix/atomix/api/runtime/indexedmap/v1"
-	lockruntimev1 "github.com/atomix/atomix/api/runtime/lock/v1"
-	mapruntimev1 "github.com/atomix/atomix/api/runtime/map/v1"
-	multimapruntimev1 "github.com/atomix/atomix/api/runtime/multimap/v1"
-	setruntimev1 "github.com/atomix/atomix/api/runtime/set/v1"
-	valueruntimev1 "github.com/atomix/atomix/api/runtime/value/v1"
-	indexedmapprotocolv1 "github.com/atomix/atomix/protocols/rsm/api/indexedmap/v1"
-	mapprotocolv1 "github.com/atomix/atomix/protocols/rsm/api/map/v1"
+	runtimev1 "github.com/atomix/atomix/api/runtime/v1"
 	rsmv1 "github.com/atomix/atomix/protocols/rsm/api/v1"
 	"github.com/atomix/atomix/protocols/rsm/pkg/client"
-	counterv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/counter/v1"
-	countermapv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/countermap/v1"
-	electionv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/election/v1"
-	indexedmapv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/indexedmap/v1"
-	lockv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/lock/v1"
-	mapv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/map/v1"
-	multimapv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/multimap/v1"
-	setv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/set/v1"
-	valuev1 "github.com/atomix/atomix/protocols/rsm/pkg/client/value/v1"
+	counterclientv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/counter/v1"
+	countermapclientv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/countermap/v1"
+	electionclientv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/election/v1"
+	indexedmapclientv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/indexedmap/v1"
+	lockclientv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/lock/v1"
+	mapclientv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/map/v1"
+	multimapclientv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/multimap/v1"
+	setclientv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/set/v1"
+	valueclientv1 "github.com/atomix/atomix/protocols/rsm/pkg/client/value/v1"
 	"github.com/atomix/atomix/runtime/pkg/network"
+	runtimecounterv1 "github.com/atomix/atomix/runtime/pkg/runtime/counter/v1"
+	runtimecountermapv1 "github.com/atomix/atomix/runtime/pkg/runtime/countermap/v1"
+	runtimeelectionv1 "github.com/atomix/atomix/runtime/pkg/runtime/election/v1"
+	runtimeindexedmapv1 "github.com/atomix/atomix/runtime/pkg/runtime/indexedmap/v1"
+	runtimelockv1 "github.com/atomix/atomix/runtime/pkg/runtime/lock/v1"
+	runtimemapv1 "github.com/atomix/atomix/runtime/pkg/runtime/map/v1"
+	runtimemultimapv1 "github.com/atomix/atomix/runtime/pkg/runtime/multimap/v1"
+	runtimesetv1 "github.com/atomix/atomix/runtime/pkg/runtime/set/v1"
+	runtimevaluev1 "github.com/atomix/atomix/runtime/pkg/runtime/value/v1"
 )
 
 func newConn(network network.Driver) *raftConn {
@@ -49,38 +48,84 @@ func (c *raftConn) Configure(ctx context.Context, config *rsmv1.ProtocolConfig) 
 	return c.ProtocolClient.Configure(ctx, *config)
 }
 
-func (c *raftConn) NewCounterV1() counterruntimev1.CounterServer {
-	return counterv1.NewCounter(c.Protocol)
+func (c *raftConn) NewCounterV1(ctx context.Context, id runtimev1.PrimitiveID) (runtimecounterv1.CounterProxy, error) {
+	proxy := counterclientv1.NewCounter(c.Protocol, id)
+	if err := proxy.Open(ctx); err != nil {
+		return nil, err
+	}
+	return proxy, nil
 }
 
-func (c *raftConn) NewCounterMapV1() countermapruntimev1.CounterMapServer {
-	return countermapv1.NewCounterMap(c.Protocol)
+func (c *raftConn) NewCounterMapV1(ctx context.Context, id runtimev1.PrimitiveID) (runtimecountermapv1.CounterMapProxy, error) {
+	proxy := countermapclientv1.NewCounterMap(c.Protocol, id)
+	if err := proxy.Open(ctx); err != nil {
+		return nil, err
+	}
+	return proxy, nil
 }
 
-func (c *raftConn) NewLeaderElectionV1() electionruntimev1.LeaderElectionServer {
-	return electionv1.NewLeaderElection(c.Protocol)
+func (c *raftConn) NewLeaderElectionV1(ctx context.Context, id runtimev1.PrimitiveID) (runtimeelectionv1.LeaderElectionProxy, error) {
+	proxy := electionclientv1.NewLeaderElection(c.Protocol, id)
+	if err := proxy.Open(ctx); err != nil {
+		return nil, err
+	}
+	return proxy, nil
 }
 
-func (c *raftConn) NewIndexedMap(config *indexedmapprotocolv1.IndexedMapConfig) (indexedmapruntimev1.IndexedMapServer, error) {
-	return indexedmapv1.NewIndexedMap(c.Protocol, config)
+func (c *raftConn) NewIndexedMapV1(ctx context.Context, id runtimev1.PrimitiveID) (runtimeindexedmapv1.IndexedMapProxy, error) {
+	proxy := indexedmapclientv1.NewIndexedMap(c.Protocol, id)
+	if err := proxy.Open(ctx); err != nil {
+		return nil, err
+	}
+	return proxy, nil
 }
 
-func (c *raftConn) NewLockV1() lockruntimev1.LockServer {
-	return lockv1.NewLock(c.Protocol)
+func (c *raftConn) NewLockV1(ctx context.Context, id runtimev1.PrimitiveID) (runtimelockv1.LockProxy, error) {
+	proxy := lockclientv1.NewLock(c.Protocol, id)
+	if err := proxy.Open(ctx); err != nil {
+		return nil, err
+	}
+	return proxy, nil
 }
 
-func (c *raftConn) NewMapV1(config *mapprotocolv1.MapConfig) (mapruntimev1.MapServer, error) {
-	return mapv1.NewMap(c.Protocol, config)
+func (c *raftConn) NewMapV1(ctx context.Context, id runtimev1.PrimitiveID) (runtimemapv1.MapProxy, error) {
+	proxy := mapclientv1.NewMap(c.Protocol, id)
+	if err := proxy.Open(ctx); err != nil {
+		return nil, err
+	}
+	return proxy, nil
 }
 
-func (c *raftConn) NewMultiMapV1() multimapruntimev1.MultiMapServer {
-	return multimapv1.NewMultiMap(c.Protocol)
+func (c *raftConn) NewMultiMapV1(ctx context.Context, id runtimev1.PrimitiveID) (runtimemultimapv1.MultiMapProxy, error) {
+	proxy := multimapclientv1.NewMultiMap(c.Protocol, id)
+	if err := proxy.Open(ctx); err != nil {
+		return nil, err
+	}
+	return proxy, nil
 }
 
-func (c *raftConn) NewSetV1() setruntimev1.SetServer {
-	return setv1.NewSet(c.Protocol)
+func (c *raftConn) NewSetV1(ctx context.Context, id runtimev1.PrimitiveID) (runtimesetv1.SetProxy, error) {
+	proxy := setclientv1.NewSet(c.Protocol, id)
+	if err := proxy.Open(ctx); err != nil {
+		return nil, err
+	}
+	return proxy, nil
 }
 
-func (c *raftConn) NewValueV1() valueruntimev1.ValueServer {
-	return valuev1.NewValue(c.Protocol)
+func (c *raftConn) NewValueV1(ctx context.Context, id runtimev1.PrimitiveID) (runtimevaluev1.ValueProxy, error) {
+	proxy := valueclientv1.NewValue(c.Protocol, id)
+	if err := proxy.Open(ctx); err != nil {
+		return nil, err
+	}
+	return proxy, nil
 }
+
+var _ runtimecounterv1.CounterProvider = (*raftConn)(nil)
+var _ runtimecountermapv1.CounterMapProvider = (*raftConn)(nil)
+var _ runtimeelectionv1.LeaderElectionProvider = (*raftConn)(nil)
+var _ runtimeindexedmapv1.IndexedMapProvider = (*raftConn)(nil)
+var _ runtimelockv1.LockProvider = (*raftConn)(nil)
+var _ runtimemapv1.MapProvider = (*raftConn)(nil)
+var _ runtimemultimapv1.MultiMapProvider = (*raftConn)(nil)
+var _ runtimesetv1.SetProvider = (*raftConn)(nil)
+var _ runtimevaluev1.ValueProvider = (*raftConn)(nil)
