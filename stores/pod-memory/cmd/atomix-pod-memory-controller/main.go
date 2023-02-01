b2b84177d3f3d5a9e48467758c4e1f5e8e93bb1f
Jordan Halterman <jordan.halterman@gmail.com>
2023-02-01 15:21:56 -0800

    Add controller init image to pod-memory store controller

--- stores/pod-memory/cmd/atomix-pod-memory-controller/main.go
+++ stores/pod-memory/cmd/atomix-pod-memory-controller/main.go
@@ -41,6 +41,7 @@ func getCommand() *cobra.Command {
 		Args: cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
 			namespace, _ := cmd.Flags().GetString("namespace")
+			certsDir, _ := cmd.Flags().GetString("certs")
 
 			// Get a config to talk to the apiserver
 			cfg, err := config.GetConfig()
@@ -87,6 +88,8 @@ func getCommand() *cobra.Command {
 
 			// Start the manager
 			log.Info("Starting the Manager")
+			mgr.GetWebhookServer().Port = 443
+			mgr.GetWebhookServer().CertDir = certsDir
 			if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
 				log.Error(err, "controller exited non-zero")
 				os.Exit(1)
@@ -94,6 +97,7 @@ func getCommand() *cobra.Command {
 		},
 	}
 	cmd.Flags().StringP("namespace", "n", "", "the namespace in which to run the controller")
+	cmd.Flags().String("certs", "/etc/atomix/certs", "the path to the certificates directory")
 	return cmd
 }
 
