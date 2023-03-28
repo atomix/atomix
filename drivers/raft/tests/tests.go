02e6ec07933806118e5891e9e6795a1af1042727
Jordan Halterman <jordan.halterman@gmail.com>
2023-03-28 11:31:31 -0700

    Use separate RaftCluster for each test suite to avoid conflicts

--- drivers/raft/tests/tests.go
+++ drivers/raft/tests/tests.go
@@ -21,7 +21,7 @@ type raftTestSuite struct {
 	*raftv1beta3.RaftV1beta3Client
 }
 
-func (s *raftTestSuite) setup(ctx context.Context, namespace string, config *rest.Config) error {
+func (s *raftTestSuite) setup(ctx context.Context, namespace, name string, config *rest.Config) error {
 	atomixV3beta4Client, err := atomixv3beta4.NewForConfig(config)
 	if err != nil {
 		return err
@@ -36,7 +36,7 @@ func (s *raftTestSuite) setup(ctx context.Context, namespace string, config *res
 
 	raftCluster := &v1beta3.RaftCluster{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "raft",
+			Name: name,
 		},
 		Spec: v1beta3.RaftClusterSpec{
 			Image:           "atomix/raft-node:latest",
@@ -50,11 +50,11 @@ func (s *raftTestSuite) setup(ctx context.Context, namespace string, config *res
 
 	raftStore := &v1beta3.RaftStore{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "raft",
+			Name: name,
 		},
 		Spec: v1beta3.RaftStoreSpec{
 			Cluster: corev1.ObjectReference{
-				Name: "raft",
+				Name: name,
 			},
 		},
 	}
@@ -65,13 +65,13 @@ func (s *raftTestSuite) setup(ctx context.Context, namespace string, config *res
 
 	storageProfile := &v3beta4.StorageProfile{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "raft",
+			Name: name,
 		},
 		Spec: v3beta4.StorageProfileSpec{
 			Routes: []v3beta4.Route{
 				{
 					Store: corev1.ObjectReference{
-						Name: "raft",
+						Name: name,
 					},
 				},
 			},
@@ -84,16 +84,16 @@ func (s *raftTestSuite) setup(ctx context.Context, namespace string, config *res
 	return nil
 }
 
-func (s *raftTestSuite) tearDown(ctx context.Context, namespace string) error {
-	err := s.StorageProfiles(namespace).Delete(ctx, "raft", metav1.DeleteOptions{})
+func (s *raftTestSuite) tearDown(ctx context.Context, namespace, name string) error {
+	err := s.StorageProfiles(namespace).Delete(ctx, name, metav1.DeleteOptions{})
 	if err != nil {
 		return err
 	}
-	err = s.RaftStores(namespace).Delete(ctx, "raft", metav1.DeleteOptions{})
+	err = s.RaftStores(namespace).Delete(ctx, name, metav1.DeleteOptions{})
 	if err != nil {
 		return err
 	}
-	err = s.RaftClusters(namespace).Delete(ctx, "raft", metav1.DeleteOptions{})
+	err = s.RaftClusters(namespace).Delete(ctx, name, metav1.DeleteOptions{})
 	if err != nil {
 		return err
 	}
@@ -106,13 +106,13 @@ type CounterTestSuite struct {
 }
 
 func (s *CounterTestSuite) SetupSuite(ctx context.Context) {
-	s.NoError(s.raftTestSuite.setup(ctx, s.Namespace(), s.Config()))
+	s.NoError(s.raftTestSuite.setup(ctx, s.Namespace(), "raft-counter-test", s.Config()))
 	s.CounterTestSuite.SetupSuite(ctx)
 }
 
 func (s *CounterTestSuite) TearDownSuite(ctx context.Context) {
 	s.CounterTestSuite.TearDownSuite(ctx)
-	s.NoError(s.raftTestSuite.tearDown(ctx, s.Namespace()))
+	s.NoError(s.raftTestSuite.tearDown(ctx, s.Namespace(), "raft-counter-test"))
 }
 
 type MapTestSuite struct {
@@ -121,11 +121,11 @@ type MapTestSuite struct {
 }
 
 func (s *MapTestSuite) SetupSuite(ctx context.Context) {
-	s.NoError(s.raftTestSuite.setup(ctx, s.Namespace(), s.Config()))
+	s.NoError(s.raftTestSuite.setup(ctx, s.Namespace(), "raft-map-test", s.Config()))
 	s.MapTestSuite.SetupSuite(ctx)
 }
 
 func (s *MapTestSuite) TearDownSuite(ctx context.Context) {
 	s.MapTestSuite.TearDownSuite(ctx)
-	s.NoError(s.raftTestSuite.tearDown(ctx, s.Namespace()))
+	s.NoError(s.raftTestSuite.tearDown(ctx, s.Namespace(), "raft-map-test"))
 }
